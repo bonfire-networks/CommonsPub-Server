5aa3b0267f523a9d53bf3cf936d09b418b63ba4b
ivan minutillo <minutillo.ivan@gmail.com>
2018-12-10 10:32:07 +0100

    added wrapper to sidebar

--- src/components/chrome/Nav/Nav.tsx
+++ src/components/chrome/Nav/Nav.tsx
@@ -23,6 +23,10 @@ const SubNavItemHeader = styled(SubNavItem)<any>`
   font-size: 1.25em !important;
 `;
 
+const SidebarWrapper = styled.div`
+  width: 240px;
+  display: flex;
+`;
 enum NavItems {
   Communities,
   Collections
@@ -156,7 +160,7 @@ class Nav extends React.Component<RouterProps, {}> {
 
   render() {
     return (
-      <>
+      <SidebarWrapper>
         <ZenNav expanded={this.state.open}>
           <NavItem
             title="Communities"
@@ -185,7 +189,7 @@ class Nav extends React.Component<RouterProps, {}> {
             current: this.state.activeSubNav
           })}
         </SubNav>
-      </>
+      </SidebarWrapper>
     );
   }
 }
