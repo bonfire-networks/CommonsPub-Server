4424c024e8928f7a037010b392192911bf8b596f
ivan minutillo <minutillo.ivan@gmail.com>
2018-12-22 18:44:33 +0100

    sidebar

--- src/components/chrome/Nav/Nav.tsx
+++ src/components/chrome/Nav/Nav.tsx
@@ -1,109 +1,18 @@
 import * as React from 'react';
 import { withRouter } from 'react-router';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  Nav as ZenNav,
-  NavItem,
-  NavItemText,
-  NavItemIcon,
-  SubNav,
-  SubNavItem,
-  SubNavItemText
-} from '@zendeskgarden/react-chrome';
-import { faTh, faUsers } from '@fortawesome/free-solid-svg-icons';
 import { RouterProps } from 'react-router';
-
+import { NavLink } from 'react-router-dom';
+import Logo from '../../brand/Logo/Logo';
 import styled from '../../../themes/styled';
-
-const SubNavItemHeader = styled(SubNavItem)<any>`
-  font-weight: bold;
-  pointer-events: none;
-  color: black !important;
-  text-shadow: 1px 1px 0 rgba(255, 151, 0, 0.4);
-  font-size: 1.25em !important;
-`;
 
 const SidebarWrapper = styled.div`
   width: 240px;
   display: flex;
+  flex-direction: column;
+  padding: 16px;
+  position: relative;
+  background: ${props => props.theme.styles.colour.primary};
 `;
-enum NavItems {
-  Communities,
-  Collections
-}
-
-enum SubNavItems {
-  Featured,
-  Yours,
-  Following
-}
-
-const navPathnames = {
-  [NavItems.Communities]: '/communities',
-  [NavItems.Collections]: '/collections'
-};
-
-const subNavPathnames = {
-  [SubNavItems.Featured]: '/featured',
-  [SubNavItems.Yours]: '/yours',
-  [SubNavItems.Following]: '/following'
-};
-
-const navMatch = new Map([
-  [/^\/communities/, NavItems.Communities],
-  [/^\/collections/, NavItems.Collections]
-]);
-
-const subNavMatch = new Map([
-  [/^\/communities\/?$/, SubNavItems.Featured],
-  [/^\/collections\/?$/, SubNavItems.Featured],
-  [/^\/(communities|collections)\/featured/, SubNavItems.Featured],
-  [/^\/(communities|collections)\/following/, SubNavItems.Following],
-  [/^\/(communities|collections)\/yours/, SubNavItems.Yours]
-]);
-
-const subNavItems = {
-  [NavItems.Communities]: ({ current, onClick }) => (
-    <>
-      <SubNavItemHeader>Communities</SubNavItemHeader>
-      <SubNavItem
-        current={current === SubNavItems.Featured}
-        onClick={() => onClick(SubNavItems.Featured)}
-      >
-        <SubNavItemText>Featured</SubNavItemText>
-      </SubNavItem>
-      <SubNavItem
-        current={current === SubNavItems.Yours}
-        onClick={() => onClick(SubNavItems.Yours)}
-      >
-        <SubNavItemText>Yours</SubNavItemText>
-      </SubNavItem>
-    </>
-  ),
-  [NavItems.Collections]: ({ current, onClick }) => (
-    <>
-      <SubNavItemHeader>Collections</SubNavItemHeader>
-      <SubNavItem
-        current={current === SubNavItems.Featured}
-        onClick={() => onClick(SubNavItems.Featured)}
-      >
-        <SubNavItemText>Featured</SubNavItemText>
-      </SubNavItem>
-      <SubNavItem
-        current={current === SubNavItems.Following}
-        onClick={() => onClick(SubNavItems.Following)}
-      >
-        <SubNavItemText>Following</SubNavItemText>
-      </SubNavItem>
-      <SubNavItem
-        current={current === SubNavItems.Yours}
-        onClick={() => onClick(SubNavItems.Yours)}
-      >
-        <SubNavItemText>Yours</SubNavItemText>
-      </SubNavItem>
-    </>
-  )
-};
 
 /**
  * Left-side navigation menu that is always present, allows user to view
@@ -111,92 +20,157 @@ const subNavItems = {
  * communities.
  */
 class Nav extends React.Component<RouterProps, {}> {
-  state: any = {
-    open: false,
-    activeNav: null,
-    activeSubNav: null
-  };
-
-  constructor(props) {
-    super(props);
-
-    for (const [re, item] of navMatch) {
-      if (re.test(props.location.pathname)) {
-        this.state.activeNav = item;
-      }
-    }
-    for (const [re, item] of subNavMatch) {
-      if (re.test(props.location.pathname)) {
-        this.state.activeSubNav = item;
-      }
-    }
-
-    if (!this.state.activeNav) {
-      this.state.activeNav = NavItems.Communities;
-    }
-
-    this.toggleSubNav = this.toggleSubNav.bind(this);
-  }
-
-  toggleMenu() {
-    this.setState({
-      open: !this.state.open
-    });
-  }
-
-  toggleNav(nav) {
-    this.setState({
-      activeNav: nav,
-      activeSubNav: SubNavItems.Featured
-    });
-    this.props.history.push(
-      navPathnames[nav] + subNavPathnames[SubNavItems.Featured]
-    );
-  }
-
-  toggleSubNav(subNav) {
-    this.setState({
-      activeSubNav: subNav
-    });
-    this.props.history.push(
-      navPathnames[this.state.activeNav] + subNavPathnames[subNav]
-    );
-  }
-
   render() {
     return (
       <SidebarWrapper>
-        <ZenNav expanded={this.state.open}>
-          <NavItem
-            title="Communities"
-            onClick={() => this.toggleNav(NavItems.Communities)}
-            current={this.state.activeNav === NavItems.Communities}
+        <Logo />
+        <NavList>
+          <NavLink
+            isActive={(match, location) => {
+              return location.pathname === `/`;
+            }}
+            activeStyle={{
+              position: 'relative',
+              color: '#fff !important'
+            }}
+            to={'/'}
           >
-            <NavItemIcon>
-              <FontAwesomeIcon icon={faUsers} />
-            </NavItemIcon>
-            <NavItemText>Communities</NavItemText>
-          </NavItem>
-          <NavItem
-            title="Collections"
-            onClick={() => this.toggleNav(NavItems.Collections)}
-            current={this.state.activeNav === NavItems.Collections}
+            <Item>Home</Item>
+          </NavLink>
+        </NavList>
+        <NavList>
+          <Title>Communities</Title>
+          <NavLink
+            isActive={(match, location) => {
+              return (
+                location.pathname === `/communities/featured/` ||
+                location.pathname === `/communities/featured`
+              );
+            }}
+            activeStyle={{
+              position: 'relative',
+              color: '#fff !important'
+            }}
+            to={'/communities/featured'}
           >
-            <NavItemIcon>
-              <FontAwesomeIcon icon={faTh} />
-            </NavItemIcon>
-            <NavItemText>Collections</NavItemText>
-          </NavItem>
-        </ZenNav>
-        <SubNav>
-          {subNavItems[this.state.activeNav]({
-            onClick: this.toggleSubNav,
-            current: this.state.activeSubNav
-          })}
-        </SubNav>
+            <Item>Featured</Item>
+          </NavLink>
+          <NavLink
+            isActive={(match, location) => {
+              return (
+                location.pathname === `/communities` ||
+                location.pathname === `/communities/`
+              );
+            }}
+            activeStyle={{
+              position: 'relative',
+              color: '#fff !important'
+            }}
+            to={'/communities'}
+          >
+            <Item>Yours</Item>
+          </NavLink>
+        </NavList>
+        <NavList>
+          <Title>Collections</Title>
+          <NavLink
+            isActive={(match, location) => {
+              return (
+                location.pathname === `/collections/featured` ||
+                location.pathname === `/collections/featured/`
+              );
+            }}
+            activeStyle={{
+              position: 'relative',
+              color: '#fff !important'
+            }}
+            to={'/collections/featured'}
+          >
+            <Item>Featured</Item>
+          </NavLink>
+          <NavLink
+            isActive={(match, location) => {
+              return (
+                location.pathname === `/collections/following/` ||
+                location.pathname === `/collections/following`
+              );
+            }}
+            activeStyle={{
+              position: 'relative',
+              color: '#fff !important'
+            }}
+            to={'/collections/following'}
+          >
+            <Item>Following</Item>
+          </NavLink>
+          <NavLink
+            isActive={(match, location) => {
+              return (
+                location.pathname === `/collections/` ||
+                location.pathname === `/collections`
+              );
+            }}
+            activeStyle={{
+              position: 'relative',
+              color: '#fff !important'
+            }}
+            to={'/collections'}
+          >
+            <Item>Yours</Item>
+          </NavLink>
+        </NavList>
+
+        <Bottom>New Community</Bottom>
       </SidebarWrapper>
     );
   }
 }
+
+const Bottom = styled.div`
+  position: absolute;
+  bottom: 0;
+  height: 60px;
+  border-top: 1px solid rgba(250, 250, 250, 0.2);
+  left: 0;
+  right: 0;
+  line-height: 60px;
+  color: #fff;
+  font-size: 13px;
+  text-align: center;
+`;
+
+const NavList = styled.div`
+  margin-bottom: 24px;
+  & a {
+    text-decoration: none;
+    color: #ffffffb5;
+    &: before {
+      position: absolute;
+      content: '';
+      left: -16px;
+      top: 0;
+      bottom: 0;
+      width: 4px;
+      display: block;
+      background: #00ffca;
+      height: 20px;
+    }
+  }
+`;
+const Item = styled.div`
+  font-size: 13px;
+  font-weight: 600;
+  color: #ffffffd4;
+  margin-bottom: 8px;
+  letter-spacing: 1px;
+`;
+const Title = styled.div`
+  font-size: 11px;
+  text-transform: uppercase;
+  font-weight: 500;
+  margin-bottom: 12px;
+  letter-spacing: 0.5px;
+  color: #ffffffa1;
+`;
 
 export default withRouter(Nav as any);
