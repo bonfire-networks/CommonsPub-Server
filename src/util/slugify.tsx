a92cc53b0275dbd8782c11d47bd00fb55c36056c
Mayel <git@mayel.space>
2019-01-03 10:44:19 +0200

    fix for empty str

--- src/util/slugify.tsx
+++ src/util/slugify.tsx
@@ -1,6 +1,7 @@
 // TODO remove when we start using real data
 export default function slugify(str: string): string {
   return str
+    .toString()
     .replace(/[^a-z0-9 ]/gi, '')
     .replace(/\s+/g, '-')
     .toLowerCase();
