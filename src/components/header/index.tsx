cc20e5e1df14083720d924983be0b4e0c72d816f
ivan <bernini@inventati.org>
2019-01-07 10:20:17 +0100

    menu settings on header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,8 +3,11 @@ import styled from '../../themes/styled';
 import Text from '../inputs/Text/Text';
 import { clearFix } from 'polished';
 import Avatar from '../elements/Avatar/Avatar';
-
-interface Props {}
+import { compose, withHandlers, withState } from 'recompose';
+interface Props {
+  handleOpen(): boolean;
+  isOpen: boolean;
+}
 
 const Header: React.SFC<Props> = props => (
   <Wrapper>
@@ -13,17 +16,54 @@ const Header: React.SFC<Props> = props => (
     </Left>
     <Right>
       <Avatar>
-        <img src="https://picsum.photos/100/100?random" alt="Example avatar" />
+        <img
+          onClick={props.handleOpen}
+          src="https://picsum.photos/100/100?random"
+          alt="Example avatar"
+        />
       </Avatar>
     </Right>
+    {props.isOpen ? (
+      <Menu>
+        <List>
+          <Item>Edit profile</Item>
+          <Item>Settings</Item>
+        </List>
+        <List>
+          <Item>Sign out</Item>
+        </List>
+      </Menu>
+    ) : null}
   </Wrapper>
 );
 
 const Wrapper = styled.div`
   height: 50px;
   background: #fff;
-  box-shadow: 0 2px 2px rgba(0, 0, 0, 0.1);
+  box-shadow: 0 1px 1px rgba(0, 0, 0, 0.1);
   ${clearFix()};
+  position: relative;
+`;
+
+const Menu = styled.div`
+  background: #fff;
+  position: absolute;
+  border-radius: 4px;
+  top: 60px;
+  width: 140px;
+  right: 10px;
+  box-shadow: 0 1px 1px rgba(0, 0, 0, 0.1);
+`;
+const List = styled.div`
+  padding: 8px;
+  border-bottom: 1px solid #dadada;
+`;
+const Item = styled.div`
+  font-size: 14px;
+  line-height: 30px;
+  height: 30px;
+  font-weight: 600;
+  color: rgba(0, 0, 0, 0.6);
 `;
 const Left = styled.div`
   float: left;
@@ -47,6 +87,14 @@ const Right = styled.div`
   float: right;
   margin-top: 5px;
   margin-right: 8px;
+  & img {
+    cursor: pointer;
+  }
 `;
 
-export default Header;
+export default compose(
+  withState('isOpen', 'onOpen', false),
+  withHandlers({
+    handleOpen: props => props.onOpen(!props.isOpen)
+  })
+)(Header);
