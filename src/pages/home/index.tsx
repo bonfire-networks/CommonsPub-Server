463d60f99e6e4d19f16bd97a62aacc899dccded4
ivan minutillo <minutillo.ivan@gmail.com>
2019-01-25 18:52:04 +0100

    minor UI @ homepage

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -9,60 +9,87 @@ interface Props {
 }
 
 const Home: React.SFC<Props> = props => (
-  <Wrapper>
-    <Title>👋 Welcome {props.data.user.data.name}!</Title>
-    <p>
-      Thanks for being part of this testing process for MoodleNet. <br />
-      <Video>
-        <iframe
-          style={{
-            border: 'none',
-            padding: '5px',
-            borderRadius: '0.25em',
-            backgroundColor: 'rgb(232,232,232)'
-          }}
-          data-allowfullscreen
-          width="600"
-          height="337"
-          scrolling="no"
-          data-frameborder="0"
-          src="https://www.wevideo.com/view/1290471043"
-        />
-      </Video>
-      We’re trying to discover the answer to this question:
-    </p>
-    <blockquote>
-      Do educators want to join communities to curate collections of resources?
-    </blockquote>
-    <p>
-      During this testing period, we’ll observe what you choose to curate and
-      comment in MoodleNet, and listening to what you tell us about your
-      experiences.
-      <br />
-      Although you’re one of only 100 people who have an account to be involved
-      in the initial testing of MoodleNet, this isn’t a ‘closed’ environment.
-      The resources you curate and the discussions you engage in are likely to
-      live on and will be publicly viewable. <br />
-      We’re using{' '}
-      <a target="blank" href="https://changemap.co/moodle/moodlenet">
-        Changemap
-      </a>{' '}
-      to collect your feedback during this test, so please do use that to
-      suggest everything from small tweaks to major changes!
-      <br />
-      We’re really looking forward to seeing how you use MoodleNet to share and
-      curate resources, and collaborate with other educators!
-      <br />
-      <b>Doug, Mayel, Alex and Ivan</b> <br />
-      <i>MoodleNet Team</i> <br />
-      <u>
-        PS. Don’t worry, we’re not tracking you! By ‘observe you’ we just mean
-        checking out what you curate and comment on that others can see.
-      </u>
-    </p>
-  </Wrapper>
+  <Container>
+    <Wrapper>
+      <Title>👋 Welcome {props.data.user.data.name}!</Title>
+      <p>
+        Thanks for being part of this testing process for MoodleNet. <br />
+        <Video>
+          <iframe
+            style={{
+              border: 'none',
+              padding: '5px',
+              borderRadius: '0.25em',
+              backgroundColor: 'rgb(232,232,232)'
+            }}
+            data-allowfullscreen
+            width="600"
+            height="337"
+            scrolling="no"
+            data-frameborder="0"
+            src="https://www.wevideo.com/view/1290471043"
+          />
+        </Video>
+        We’re trying to discover the answer to this question:
+      </p>
+      <blockquote>
+        Do educators want to join communities to curate collections of
+        resources?
+      </blockquote>
+      <p>
+        During this testing period, we’ll observe what you choose to curate and
+        comment in MoodleNet, and listening to what you tell us about your
+        experiences.
+        <br />
+        Although you’re <b>one of only 100 people</b> who have an account to be
+        involved in the initial testing of MoodleNet,{' '}
+        <i>this isn’t a closed environment.</i>
+        The resources you curate and the discussions you engage in are likely to
+        live on and will be publicly viewable. <br />
+        We’re using{' '}
+        <a target="blank" href="https://changemap.co/moodle/moodlenet">
+          Changemap
+        </a>{' '}
+        to collect your feedback during this test, so please do use that to
+        suggest everything from small tweaks to major changes!
+        <br />
+        We’re really looking forward to seeing how you use MoodleNet to share
+        and curate resources, and collaborate with other educators!
+      </p>
+      <Sign>
+        <b>Doug, Mayel, Alex and Ivan</b> <br />
+        <i>MoodleNet Team</i> <br />
+      </Sign>
+      <p>
+        <u>
+          PS. Don’t worry, we’re not tracking you! By ‘observe you’ we just mean
+          checking out what you curate and comment on that others can see.
+        </u>
+      </p>
+    </Wrapper>
+  </Container>
 );
 
+const Container = styled.div`
+  overflow: overlay;
+`;
+const Sign = styled.div`
+  & b {
+    margin-top: 40px;
+    display: block;
+    margin-bottom: 0;
+  }
+
+  & i {
+    display: block;
+    margin-top: -20px;
+    font-weight: 700;
+    color: #c1c1c1;
+    font-style: normal;
+    font-size: 14px;
+    letter-spacing: 1px;
+  }
+`;
 const Video = styled.div`
   margin: 16px 0;
 `;
@@ -70,7 +97,7 @@ const Wrapper = styled.div`
   width: 620px;
   margin: 0 auto;
   margin-bottom: 40px;
-  overflow: overlay;
+
   & a {
     color: #f98011;
     text-decoration: none;
@@ -92,7 +119,7 @@ const Wrapper = styled.div`
     font-size: 16px;
     letter-spacing: 0;
     color: #3c3c3c;
-    line-height: 24px;
+    line-height: 30px;
   }
   & blockquote {
     font-size: 22px;
@@ -101,22 +128,6 @@ const Wrapper = styled.div`
     padding-left: 20px;
     margin-left: 20px;
     color: #f98011;
-  }
-
-  & b {
-    margin-top: 40px;
-    display: block;
-    margin-bottom: 0;
-  }
-
-  & i {
-    display: block;
-    margin-top: -20px;
-    font-weight: 700;
-    color: #c1c1c1;
-    font-style: normal;
-    font-size: 14px;
-    letter-spacing: 1px;
   }
 
   & u {
