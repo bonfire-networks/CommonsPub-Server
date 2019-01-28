da359089c3484fa35c036452aec3a3455be30257
ivan minutillo <minutillo.ivan@gmail.com>
2019-01-28 12:43:10 +0100

    community elemenet

--- src/components/elements/Community/Community.tsx
+++ src/components/elements/Community/Community.tsx
@@ -3,6 +3,7 @@ import styled from '../../../themes/styled';
 import H5 from '../../typography/H5/H5';
 // import P from '../../typography/P/P';
 import { Link } from 'react-router-dom';
+import { Preferites } from '../Icons';
 
 interface Props {
   title: string;
@@ -21,6 +22,16 @@ const Community: React.SFC<Props> = ({
     <Link to={`communities/${id}`}>
       <WrapperImage>
         <Img style={{ backgroundImage: `url(${icon})` }} />
+        <Overlay>
+          <Span>
+            <Preferites
+              width={32}
+              height={32}
+              strokeWidth={1}
+              color={'#f0f0f0'}
+            />
+          </Span>
+        </Overlay>
       </WrapperImage>
       <H5>{title}</H5>
     </Link>
@@ -33,6 +44,35 @@ const Community: React.SFC<Props> = ({
 
 export default Community;
 
+const Overlay = styled.span`
+  position: absolute;
+  background: rgba(0, 0, 0, 0.5);
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  grid-template-columns: 1fr;
+  grid-column-gap: 8px;
+  display: none;
+`;
+const Span = styled.div`
+  text-align: center;
+  border-radius: 100px;
+  width: 50px;
+  height: 50px;
+  text-align: center;
+  cursor: pointer;
+  margin: 0 auto;
+  margin-top: 80px;
+  & svg {
+    margin-top: 8px;
+    text-align: center;
+  }
+  &:hover  {
+    background: rgba(0, 0, 0, 0.7);
+  }
+`;
+
 const Wrapper = styled.div`
   & h5 {
     margin: 0;
@@ -43,7 +83,14 @@ const Wrapper = styled.div`
     color: inherit;
   }
 `;
-const WrapperImage = styled.div``;
+const WrapperImage = styled.div`
+  position: relative;
+  &:hover {
+    & span {
+      display: grid;
+    }
+  }
+`;
 const Img = styled.div`
   height: 200px;
   background-size: cover;
