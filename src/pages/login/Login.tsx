db7799d8a1d9dad341a534e1ad46767030c23cb1
Mayel <git@mayel.space>
2019-02-11 12:34:35 +0200

    Revert "adapt updated mequery client side"
    This reverts commit ed356073e2fe3b0361dec0f191fe1b5af8fa1d60.

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -17,7 +17,7 @@ import Body from '../../components/chrome/Body/Body';
 import H6 from '../../components/typography/H6/H6';
 // import P from '../../components/typography/P/P';
 import LoginForm from './LoginForm';
-// import User from '../../types/User';
+import User from '../../types/User';
 import { ValidationField, ValidationObject, ValidationType } from './types';
 
 const { getUserQuery } = require('../../graphql/getUser.client.graphql');
@@ -235,7 +235,6 @@ class Login extends React.Component<LoginProps, LoginState> {
   }
 
   render() {
-    console.log(this.props);
     if (this.state.redirectTo) {
       return <Redirect to={this.state.redirectTo as any} />;
     }
@@ -312,7 +311,7 @@ class Login extends React.Component<LoginProps, LoginState> {
 export interface Args {
   data: {
     isAuthenticated: boolean;
-    user: any;
+    user: User;
   };
 }
 
