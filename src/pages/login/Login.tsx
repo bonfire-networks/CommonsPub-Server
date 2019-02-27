f85feccd6acfec0f5e2504ef4359fe4d0d279edf
bernini <bernini@inventati.org>
2019-02-27 12:46:16 +0100

    message on login

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -76,18 +76,18 @@ const BodyCenterContent = styled(Body)`
   flex-direction: column;
 `;
 
-// const Banner = styled.div`
-//   flex: 1;
-//   background: #ffc9c9;
-//   width: 100%;
-//   text-align: center;
-//   font-size: 15px;
-//   color: #5e1919;
-//   height: 60px;
-//   line-height: 60px;
-//   font-weight: 700;
-//   max-height: 60px;
-// `;
+const Banner = styled.div`
+  flex: 1;
+  background: #ffc9c9;
+  width: 100%;
+  text-align: center;
+  font-size: 15px;
+  color: #5e1919;
+  height: 60px;
+  line-height: 60px;
+  font-weight: 700;
+  max-height: 60px;
+`;
 
 /**
  * @param Component
@@ -244,12 +244,9 @@ class Login extends React.Component<LoginProps, LoginState> {
     return (
       <>
         <BodyCenterContent>
-          {/* <Banner>
-            <Trans>
-              We're having some teething problems right now, but will be back
-              soon! Keep checking back.
-            </Trans>
-          </Banner> */}
+          <Banner>
+            <Trans>Seeing some error messages? Just hit refresh!</Trans>
+          </Banner>
           <Head>
             <Col size={12}>
               <Left>
