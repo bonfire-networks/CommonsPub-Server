7f654002b1c12e94fa4d64129e2e5c116ba03819
ivan minutillo <minutillo.ivan@gmail.com>
2019-03-12 09:43:02 +0100

    button on logo

--- src/components/brand/Logo/Logo.tsx
+++ src/components/brand/Logo/Logo.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 
 import styled from '../../../themes/styled';
 import { Link } from 'react-router-dom';
+import { Trans } from '@lingui/macro';
 
 const LogoH1 = styled.h1`
   margin: 0;
@@ -22,6 +23,19 @@ const LogoH1 = styled.h1`
   }
 `;
 
+const Small = styled.a`
+  margin-left: 4px;
+  padding: 4px 8px;
+  border-radius: 4px;
+  background: ${props => props.theme.styles.colour.primary};
+  color: white !important;
+  font-weight: 500 !important;
+  letter-spacing: 0.5px;
+  font-size: 11px;
+  text-transform: uppercase;
+  cursor: pointer;
+`;
+
 type LogoProps = {
   link?: boolean;
 };
@@ -36,6 +50,13 @@ export default ({ link = true }: LogoProps) => {
       <Link to="/" title="MoodleNet">
         MoodleNet <small>v0.5 alpha</small>
       </Link>
+      <Small
+        href="https://blog.moodle.net/2019/moodlenet-0-5-alpha-update/"
+        target="blank"
+      >
+        🎉
+        <Trans>NEW</Trans>
+      </Small>
     </LogoH1>
   );
 };
