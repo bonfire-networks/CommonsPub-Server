f87aa9c50984d1cf50be44a7580bf0499ba64fe2
Alex Castaño <alexcastanodev@gmail.com>
2019-05-03 11:08:24 +0200

    Bump version 0.0.18

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MoodleNet.Mixfile do
   def project do
     [
       app: :moodle_net,
-      version: "0.0.17",
+      version: "0.0.18",
       elixir: "~> 1.7.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
