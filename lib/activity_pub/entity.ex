8287ac540bfeaf44b06350f48a2efec7ebbdbf2b
Mayel <git@mayel.space>
2019-05-17 23:05:46 +0200

    started documenting for exdoc & added CI to build the docs

--- lib/activity_pub/entity.ex
+++ lib/activity_pub/entity.ex
@@ -1,4 +1,15 @@
 defmodule ActivityPub.Entity do
+  @moduledoc """
+
+`Entity` is used to refer to any ActivityPub `Object` or `Link` in the library—ActivityPub `Object` and `Link` are disjoint, they are different things. An AP `Entity` is just a map which also has the field `__ap__` set to a `ActivityPub.Metadata` struct.
+
+**Note:** Elixir Structs couldn't be used because they have fixed fields, which would produce compilation errors in case of unknown properties.
+
+In addition to the `Metadata` struct in the `__ap__` field, the map it also stores every property it has, like in JSON. This allows working with the `Entity` in simple ways:
+- actor.followers
+- actor[:followers]
+  """
+
   require ActivityPub.Guards, as: APG
 
   alias ActivityPub.{Metadata}
@@ -34,6 +45,13 @@ defmodule ActivityPub.Entity do
     end)
   end
 
+  @doc """
+  If the entity uses any properties that aren't part of ActivityStreams' [standard vocabulary](https://www.w3.org/TR/activitystreams-vocabulary/), but are instead used as [extensions](https://www.w3.org/TR/activitystreams-core/#extensibility) to the vocabulary (MoodleNet-specific ones or otherwise), those are stored using strings instead of atoms: `actor["extension_field"]`
+
+  There are two reasons:
+  1. We have a clear difference between extension fields and regular fields defined by an _aspect_.
+  2. We avoid the "atom overload" attack—the BEAM Garbage Collector doesn't collect atoms.
+  """
   def extension_fields(entity) when APG.is_entity(entity) do
     Enum.reduce(entity, %{}, fn
       {key, _}, acc when is_atom(key) -> acc
