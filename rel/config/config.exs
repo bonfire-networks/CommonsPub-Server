af0701e5b35b8abe8d33eb4da06d578b7f3296e0
James Laver <james.laver@gmail.com>
2019-05-30 12:48:46 +0200

    Make runtime config work

--- rel/config/config.exs
+++ rel/config/config.exs
@@ -1,27 +1,27 @@
 use Mix.Config
 
-defp env(name) do
+env = fn name ->
   case System.get_env(name) do
     nil -> throw {:missing_env_var, name}
     other -> other
   end
 end
-
+  
 config :moodle_net, MoodleNet.Repo,
-  username: env("DATABASE_USER"),
-  password: env("DATABASE_PASS"),
-  database: env("DATABASE_NAME"),
-  hostname: env("DATABASE_HOST"),
+  username: env.("DATABASE_USER"),
+  password: env.("DATABASE_PASS"),
+  database: env.("DATABASE_NAME"),
+  hostname: env.("DATABASE_HOST"),
   pool_size: 15
-
-port = String.to_integer(env("PORT") || "8080")
+  
+port = String.to_integer(env.("PORT") || "8080")
 config :moodle_net, MoodleNetWeb.Endpoint,
   http: [port: port],
-  url: [host: env("HOSTNAME"), port: port],
+  url: [host: env.("HOSTNAME"), port: port],
   root: ".",
-  secret_key_base: env("SECRET_KEY_BASE")
-
-config :moodle_net, :ap_base_url, env("AP_BASE_URL")
-
-config :moodle_net, :mail_domain, env("MAIL_DOMAIN")
-config :moodle_net, :mail_key, env("MAIL_KEY")
+  secret_key_base: env.("SECRET_KEY_BASE")
+  
+config :moodle_net, :ap_base_url, env.("AP_BASE_URL")
+  
+config :moodle_net, :mail_domain, env.("MAIL_DOMAIN")
+config :moodle_net, :mail_key, env.("MAIL_KEY")
