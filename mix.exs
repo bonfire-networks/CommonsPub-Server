279b2b160740888e2222ea9e3994ab307b3fc3d6
Mayel <git@mayel.space>
2019-05-30 13:13:19 +0200

    version bump

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule MoodleNet.Mixfile do
   def project do
     [
       app: :moodle_net,
-      version: "0.0.21", # current MoodleNet Server version
+      version: "0.0.22", # current MoodleNet Server version
       elixir: "~> 1.7", # required version of Elixir
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
