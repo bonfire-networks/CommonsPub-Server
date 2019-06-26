5b157dcbfe5721606d3a0fa4c5763c87091098c7
Karen Konou <konoukaren@gmail.com>
2019-06-26 11:15:54 +0200

    Add fixtures, mock and test for object fetching

---
+++ test/activity_pub_web/fetcher_test.exs
@@ -0,0 +1,49 @@
+# MoodleNet: Connecting and empowering educators worldwide
+# Copyright © 2018-2019 Moodle Pty Ltd <https://moodle.com/moodlenet/>
+# Contains code from Pleroma <https://pleroma.social/> and CommonsPub <https://commonspub.org/>
+# SPDX-License-Identifier: AGPL-3.0-only
+
+defmodule ActivityPubWeb.FetcherTest do
+  use MoodleNet.DataCase
+  import Tesla.Mock
+
+  alias ActivityPubWeb.Fetcher
+
+  setup do
+    mock(fn
+      %{method: :get, url: "https://pleroma.example/userisgone404"} ->
+        %Tesla.Env{status: 404}
+
+      %{method: :get, url: "https://pleroma.example/userisgone410"} ->
+        %Tesla.Env{status: 410}
+
+      env ->
+        apply(HttpRequestMock, :request, [env])
+    end)
+
+    :ok
+  end
+
+  describe "fetching objects" do
+    test "fetches a pleroma note" do
+      {:ok, data} =
+        Fetcher.fetch_object_from_id(
+          "https://kawen.space/objects/eb3b1181-38cc-4eaf-ba1b-3f5431fa9779"
+        )
+
+      assert data
+    end
+  end
+
+  describe "handles errors" do
+    test "handle HTTP 410 Gone response" do
+      assert {:error, "Object has been deleted"} ==
+               Fetcher.fetch_remote_object_from_id("https://pleroma.example/userisgone410")
+    end
+
+    test "handle HTTP 404 response" do
+      assert {:error, "Object has been deleted"} ==
+               Fetcher.fetch_remote_object_from_id("https://pleroma.example/userisgone404")
+    end
+  end
+end
---
+++ test/support/http_request_mock.ex
@@ -0,0 +1,37 @@
+# MoodleNet: Connecting and empowering educators worldwide
+# Copyright © 2018-2019 Moodle Pty Ltd <https://moodle.com/moodlenet/>
+# Contains code from Pleroma <https://pleroma.social/> and CommonsPub <https://commonspub.org/>
+# SPDX-License-Identifier: AGPL-3.0-only
+
+defmodule HttpRequestMock do
+  require Logger
+
+  def request(
+        %Tesla.Env{
+          url: url,
+          method: method,
+          headers: headers,
+          query: query,
+          body: body
+        } = _env
+      ) do
+    with {:ok, res} <- apply(__MODULE__, method, [url, query, body, headers]) do
+      res
+    else
+      {_, _r} = error ->
+        # Logger.warn(r)
+        error
+    end
+  end
+
+  def get(url, query \\ [], body \\ [], headers \\ [])
+
+  def get("https://kawen.space/objects/eb3b1181-38cc-4eaf-ba1b-3f5431fa9779", _, _, _) do
+    {:ok,
+     %Tesla.Env{
+       status: 200,
+       body:
+         File.read!("test/fixtures/pleroma_note.json")
+     }}
+  end
+end
