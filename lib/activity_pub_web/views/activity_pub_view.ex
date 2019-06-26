a2a11b556246d5092be989db585475ff3f019396
James Laver <james.laver@gmail.com>
2019-06-26 14:47:14 +0200

    Comment out unused reference to unknown function in not yet used activity pub view to avoid an error

--- lib/activity_pub_web/views/activity_pub_view.ex
+++ lib/activity_pub_web/views/activity_pub_view.ex
@@ -73,6 +73,12 @@ defmodule ActivityPubWeb.ActivityPubView do
     Map.put(ret, "endpoints", endpoints)
   end
 
+  defp extension_fields(entity) do
+    entity
+    |> Entity.extension_fields()
+    |> Enum.filter(&filter_by_value/1)
+    |> Enum.map(&normalize_value/1)
+
   # FIXME this can be calculated in compilation time :)
   defp filter_fields_by_definition(aspect) do
     aspect.__aspect__(:fields)
