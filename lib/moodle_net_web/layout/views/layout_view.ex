eb3242c0a15a65a9fe9e4a71c5c7b678c6ada9b6
Antonis Kalou <kalouantonis@protonmail.com>
2019-07-02 11:42:47 +0200

    Remove some of the compilation warnings
    Some functions, variables, etc. weren't being used.
    
    There are still some errors regarding pattern matching in macros and accessing
    .__schema__. Lastly, there are some @doc blocks for private functions, which
    gives warnings.

--- lib/moodle_net_web/layout/views/layout_view.ex
+++ lib/moodle_net_web/layout/views/layout_view.ex
@@ -5,12 +5,4 @@
 
 defmodule MoodleNetWeb.LayoutView do
   use MoodleNetWeb, :view
-
-  defp logo_url() do
-    path = MoodleNetWeb.Endpoint.static_path("/images/moodlenet-logo.png")
-
-    MoodleNetWeb.Endpoint.struct_url()
-    |> Map.put(:path, path)
-    |> URI.to_string()
-  end
 end
