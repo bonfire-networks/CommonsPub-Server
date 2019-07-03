aee83b1abbc1deb2423072b13b059b8f6f213c2b
Antonis Kalou <kalouantonis@protonmail.com>
2019-07-03 18:39:48 +0200

    Add mix task to whitelist emails
    Usage: mix moodle_net.whitelist_email EMAIL
    
    *Warning*: Only intended for use on local development machine.

---
+++ lib/mix/tasks/whitelist_email.ex
@@ -0,0 +1,40 @@
+# MoodleNet: Connecting and empowering educators worldwide
+# Copyright © 2018-2019 Moodle Pty Ltd <https://moodle.com/moodlenet/>
+# Contains code from Pleroma <https://pleroma.social/> and CommonsPub <https://commonspub.org/>
+# SPDX-License-Identifier: AGPL-3.0-only
+
+defmodule Mix.Tasks.MoodleNet.WhitelistEmail do
+  use Mix.Task
+  import Mix.Ecto
+
+  @usage "mix moodle_net.whitelist_email EMAIL"
+
+  @shortdoc "Whitelist an address to allow login locally."
+  @moduledoc """
+  External developers do not typically have moodle email addresses and so need
+  to whitelist their address when signing up on a local instance.
+
+  Usage:
+
+    $ #{@usage}
+  """
+
+  def run([email | _]) when is_binary(email) do
+    Mix.Task.run("app.start")
+
+    if MoodleNet.Accounts.is_email_in_whitelist?(email) do
+      Mix.shell.info("#{email} already present in whitelist.")
+    else
+      {:ok, _} = MoodleNet.Accounts.add_email_to_whitelist(email)
+      Mix.shell.info("#{email} added to whitelist.")
+    end
+  end
+
+  def run(_args), do: Mix.shell.error("""
+  Invalid parameters.
+
+  Usage:
+
+    #{@usage}
+  """)
+end
