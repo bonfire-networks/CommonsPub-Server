713295d7b2472f5d26fce3d2ab096f94943e808b
Antonis Kalou <kalouantonis@protonmail.com>
2019-07-08 12:37:44 +0200

    Add tests for UrlBuilder.id and UrlBuilder.local?

--- test/activity_pub/url_builder_test.exs
+++ test/activity_pub/url_builder_test.exs
@@ -10,6 +10,34 @@ defmodule ActivityPub.UrlBuilderTest do
 
   @base_url Application.get_env(:moodle_net, :ap_base_url)
 
+  describe "id" do
+    test "works with regular entities" do
+      assert url = UrlBuilder.id(25)
+      assert url == "#{@base_url}/25"
+    end
+
+    test "works with pages" do
+      assert url = UrlBuilder.id({:page, 25, %{"before" => 0, "after" => 10}})
+      assert url == "#{@base_url}/25/page?after=10&before=0"
+    end
+  end
+
+  describe "local?" do
+    test "true if URL is local" do
+      assert UrlBuilder.local?("#{@base_url}/25")
+      assert UrlBuilder.local?("#{@base_url}/25/page?after=10&before=10")
+    end
+
+    test "false if URL is not local" do
+      refute UrlBuilder.local?("/25")
+      refute UrlBuilder.local?("http://not-moodle.org/25")
+    end
+
+    test "always false if value is nil" do
+      refute UrlBuilder.local?(nil)
+    end
+  end
+
   describe "get_local_id" do
     test "works with regular entities" do
       assert {:ok, 1} = UrlBuilder.get_local_id("#{@base_url}/1")
