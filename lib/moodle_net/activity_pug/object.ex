c349c3fbb4ab3838801db1ef952e355da3028552
James Laver <james.laver@gmail.com>
2019-07-10 11:39:10 +0200

    schemas for collection, comment, resource flags

---
+++ lib/moodle_net/activity_pug/object.ex
@@ -0,0 +1,16 @@
+# MoodleNet: Connecting and empowering educators worldwide
+# Copyright © 2018-2019 Moodle Pty Ltd <https://moodle.com/moodlenet/>
+# Contains code from Pleroma <https://pleroma.social/> and CommonsPub <https://commonspub.org/>
+# SPDX-License-Identifier: AGPL-3.0-only
+defmodule MoodleNet.ActivityPug.Object do
+  @moduledoc """
+  This ecto schema is just a placeholder to make real ecto models be
+  able to point at something with belongs_to.
+  You will still need to use the activitypub libraries to actually fetch this data
+  """
+  use Ecto.Schema
+
+  schema "activity_pub_objects" do
+  end
+
+end
--- priv/repo/migrations/20190703144300_flagging.exs
+++ priv/repo/migrations/20190703144300_flagging.exs
@@ -8,38 +8,33 @@ defmodule MoodleNet.Repo.Migrations.Flagging do
 
   def change do
 
-    create table(:activity_pub_object_flags) do
-      add_foreign_key(:subject_id, "activity_pub_objects")
-      add_foreign_key(:target_id, "activity_pub_objects")
-      add(:message, :text, null: false)
+    create table(:mn_collection_flags) do
+      add_foreign_key(:flagged_object_id, "activity_pub_objects")
+      add_foreign_key(:flagging_object_id, "activity_pub_objects")
+      add(:reason, :string, null: false)
+      add(:open, :boolean, null: false, default: true)
     end
 
-    alter table(:activity_pub_objects, primary_key: false) do
-      add(:flags_count, :integer, default: 0, null: false)
+    create(unique_index(:mn_collection_flags, [:flagged_object_id, :flagging_object_id], name: :mn_collection_flags_once_index))
+
+    create table(:mn_resource_flags) do
+      add_foreign_key(:flagged_object_id, "activity_pub_objects")
+      add_foreign_key(:flagging_object_id, "activity_pub_objects")
+      add(:reason, :string, null: false)
+      add(:open, :boolean, null: false, default: true)
     end
 
-    create(unique_index(:activity_pub_object_flags, [:subject_id, :target_id], name: :activity_pub_flags_unique_index))
+    create(unique_index(:mn_resource_flags, [:flagged_object_id, :flagging_object_id], name: :mn_resource_flags_once_index))
 
-    create_counter_trigger(
-      :flags_count,
-      :activity_pub_objects,
-      :local_id,
-      :activity_pub_object_flags,
-      :subject_id
-    )
-
-    alter table(:activity_pub_actor_aspects, primary_key: false) do
-      add_foreign_key(:flags_id, "activity_pub_objects", column: :local_id)
-      add(:flags_count, :integer, default: 0, null: false)
+    create table(:mn_comment_flags) do
+      add_foreign_key(:flagged_object_id, "activity_pub_objects")
+      add_foreign_key(:flagging_object_id, "activity_pub_objects")
+      add(:reason, :string, null: false)
+      add(:open, :boolean, null: false, default: true)
     end
 
-    create_counter_trigger(
-      :flags_count,
-      :activity_pub_actor_aspects,
-      :local_id,
-      :activity_pub_follows,
-      :target_id
-    )
+    create(unique_index(:mn_comment_flags, [:flagged_object_id, :flagging_object_id], name: :mn_comment_flags_once_index))
 
   end
+
 end
