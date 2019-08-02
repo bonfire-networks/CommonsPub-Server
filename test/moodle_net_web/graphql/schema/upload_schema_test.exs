9ed2978d9cfa828b154e28b8eae5de2d8c90274d
Antonis Kalou <kalouantonis@protonmail.com>
2019-08-02 10:50:16 +0200

    Add integration test for image uploading

---
+++ test/moodle_net_web/graphql/schema/upload_schema_test.exs
@@ -0,0 +1,146 @@
+# MoodleNet: Connecting and empowering educators worldwide
+# Copyright © 2018-2019 Moodle Pty Ltd <https://moodle.com/moodlenet/>
+# Contains code from Pleroma <https://pleroma.social/> and CommonsPub <https://commonspub.org/>
+# SPDX-License-Identifier: AGPL-3.0-only
+
+defmodule MoodleNetWeb.GraphQL.UploadSchemaTest do
+  use MoodleNetWeb.ConnCase, async: true
+
+  import ActivityPub.Entity, only: [local_id: 1]
+  @moduletag format: :json
+
+  def upload_query(actor, query_name, file) do
+    local_id = local_id(actor)
+    query = """
+    mutation {
+      #{query_name}(image: \"image\", localId: #{local_id})
+    }
+    """
+
+    %{
+      "query" => query,
+      "variables" => %{"image" => "file"},
+      "image" => file
+    }
+  end
+
+  describe "image" do
+    @tag :user
+    test "upload an image for an existing object", %{conn: conn, actor: actor} do
+      file = %Plug.Upload{
+        path: "test/fixtures/images/150.png",
+        filename: "150.png",
+        content_type: "image/png"
+      }
+      query = upload_query(actor, "uploadImage", file)
+
+      assert resp =
+        conn
+        |> put_req_header("content-type", "multipart/form-data")
+        |> post("/api/graphql", query)
+        |> json_response(200)
+
+      refute Map.has_key?(resp, "errors")
+      assert %{"data" => %{"uploadImage" => url}} = resp
+      assert url =~ file.filename
+    end
+
+    @tag :user
+    test "fails with an invalid file extension", %{conn: conn, actor: actor} do
+      file = %Plug.Upload{
+        path: "test/fixtures/not-a-virus.exe",
+        filename: "not-a-virus.exe",
+        content_type: "application/executable"
+      }
+      query = upload_query(actor, "uploadImage", file)
+
+      assert resp =
+        conn
+        |> put_req_header("content-type", "multipart/form-data")
+        |> post("/api/graphql", query)
+        |> json_response(200)
+
+      assert %{"errors" => [%{"message" => "invalid_file"}]} = resp
+      assert %{"data" => %{"uploadImage" => nil}} = resp
+    end
+
+    @tag :user
+    test "fails with an missing file", %{conn: conn, actor: actor} do
+      file = %Plug.Upload{
+        path: "missing.png",
+        filename: "missing.png",
+        content_type: "image/png"
+      }
+      query = upload_query(actor, "uploadImage", file)
+
+      assert resp =
+        conn
+        |> put_req_header("content-type", "multipart/form-data")
+        |> post("/api/graphql", query)
+        |> json_response(200)
+
+      assert %{"errors" => [%{"message" => "invalid_file_path"}]} = resp
+      assert %{"data" => %{"uploadImage" => nil}} = resp
+    end
+  end
+
+  describe "icon" do
+    @tag :user
+    test "upload an icon for an existing object", %{conn: conn, actor: actor} do
+      file = %Plug.Upload{
+        path: "test/fixtures/images/150.png",
+        filename: "150.png",
+        content_type: "image/png"
+      }
+      query = upload_query(actor, "uploadIcon", file)
+
+      assert resp =
+        conn
+        |> put_req_header("content-type", "multipart/form-data")
+        |> post("/api/graphql", query)
+        |> json_response(200)
+
+      refute Map.has_key?(resp, "errors")
+      assert %{"data" => %{"uploadIcon" => url}} = resp
+      assert url =~ file.filename
+    end
+
+    @tag :user
+    test "fails with an invalid file extension", %{conn: conn, actor: actor} do
+      file = %Plug.Upload{
+        path: "test/fixtures/not-a-virus.exe",
+        filename: "not-a-virus.exe",
+        content_type: "application/executable"
+      }
+      query = upload_query(actor, "uploadIcon", file)
+
+      assert resp =
+        conn
+        |> put_req_header("content-type", "multipart/form-data")
+        |> post("/api/graphql", query)
+        |> json_response(200)
+
+      assert %{"errors" => [%{"message" => "invalid_file"}]} = resp
+      assert %{"data" => %{"uploadIcon" => nil}} = resp
+    end
+
+    @tag :user
+    test "fails with an missing file", %{conn: conn, actor: actor} do
+      file = %Plug.Upload{
+        path: "missing.png",
+        filename: "missing.png",
+        content_type: "image/png"
+      }
+      query = upload_query(actor, "uploadIcon", file)
+
+      assert resp =
+        conn
+        |> put_req_header("content-type", "multipart/form-data")
+        |> post("/api/graphql", query)
+        |> json_response(200)
+
+      assert %{"errors" => [%{"message" => "invalid_file_path"}]} = resp
+      assert %{"data" => %{"uploadIcon" => nil}} = resp
+    end
+  end
+end
