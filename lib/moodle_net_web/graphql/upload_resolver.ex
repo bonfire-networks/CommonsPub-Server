f75581823aacc9bf16c307e0372fd992b5f22240
Antonis Kalou <kalouantonis@protonmail.com>
2019-08-13 10:38:10 +0200

    Add support for previews (DB only)

--- lib/moodle_net_web/graphql/upload_resolver.ex
+++ lib/moodle_net_web/graphql/upload_resolver.ex
@@ -10,23 +10,38 @@ defmodule MoodleNetWeb.GraphQL.UploadResolver do
 
   def upload_icon(params, info) do
     # FIXME: this isn't authorization
-    with {:ok, _} <- current_actor(info) do
-      do_upload(Avatar, :icon, params)
+    with {:ok, _} <- current_actor(info),
+         {:ok, object} <- fetch_object_by_id(params.local_id),
+         image_object = fetch_image_field(object, :icon),
+         {:ok, ref_url} <- Avatar.store(params.image) do
+      %{original: original_url, thumbnail: thumbnail_url} = Avatar.urls(ref_url)
+
+      with {:ok, _} <- ActivityPub.update(image_object, url: original_url),
+           {:ok, preview_object} <- update_preview(image_object, thumbnail_url),
+        do: {:ok, original_url}
     end
   end
 
   def upload_image(params, info) do
-    with {:ok, _} <- current_actor(info) do
-      do_upload(Background, :image, params)
-    end
-  end
-
-  defp do_upload(uploader, field_name, %{local_id: local_id, image: image}) do
-    with {:ok, object} <- fetch_object_by_id(local_id),
-         image_object = fetch_image_field(object, field_name),
-         {:ok, url} <- uploader.store(image),
+    with {:ok, object} <- fetch_object_by_id(params.local_id),
+         image_object = fetch_image_field(object, :image),
+         {:ok, url} <- Background.store(params.image),
          {:ok, _} <- ActivityPub.update(image_object, url: url),
       do: {:ok, url}
+  end
+
+  defp update_preview(object, url) do
+    case fetch_image_field(object, :preview) do
+      nil ->
+        params = [type: "Image", url: url, media_type: "TODO"]
+        with {:ok, preview} <- ActivityPub.new(params),
+             {:ok, preview} <- ActivityPub.insert(preview),
+             {:ok, _} <- ActivityPub.SQL.Alter.add(object, :preview, preview),
+          do: {:ok, preview}
+
+      preview ->
+        ActivityPub.update(preview, [url: url, media_type: "TODO"])
+    end
   end
 
   defp fetch_object_by_id(local_id) do
