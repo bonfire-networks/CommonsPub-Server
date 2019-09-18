1b9e0253813da02d7584926cbad643097d5df400
James Laver <james.laver@gmail.com>
2019-09-18 12:43:56 +0200

    flags and likes

--- lib/moodle_net/common/flag.ex
+++ lib/moodle_net/common/flag.ex
@@ -1,34 +1,39 @@
+# MoodleNet: Connecting and empowering educators worldwide
+# Copyright © 2018-2019 Moodle Pty Ltd <https://moodle.com/moodlenet/>
+# SPDX-License-Identifier: AGPL-3.0-only
 defmodule MoodleNet.Common.Flag do
+  @moduledoc """
+  A flag is a report that something is breaking the rules
 
-  use Ecto.Schema
+  Flags participate in the meta system and must be created from a pointer
+  """
+  use MoodleNet.Common.Schema
+  import MoodleNet.Common.Changeset, only: [meta_pointer_constraint: 1]
   alias MoodleNet.Meta.Pointer
   alias MoodleNet.Actors.Actor
   alias MoodleNet.Communities.Community
+  alias Ecto.Changeset
 
-  @primary_key {:id, :binary_id, autogenerate: false}
-  @foreign_key_type :binary_id
-  schema "mn_flag" do
+  meta_schema "mn_flag" do
     belongs_to :flagged, Pointer
     belongs_to :flagger, Actor
     belongs_to :community, Community
-    field :resolved_at, :utc_datetime
+    field :deleted_at, :utc_datetime_usec
     field :reason, :string
     timestamps()
   end
 
   @create_cast ~w(flagged_id flagger_id community_id reason)a
-  @create_required ~w(id flagged_id flagger_id reason)a
-  def create_changeset(id, attrs) do
+  @create_required ~w(flagged_id flagger_id reason)a
+
+  def create_changeset(%Pointer{id: id}, attrs) do
     %__MODULE__{id: id}
     |> Changeset.cast(attrs, @create_cast)
     |> Changeset.validate_required(@create_required)
-    |> Changeset.foreign_key_constraint(:id)
     |> Changeset.foreign_key_constraint(:flagged_id)
     |> Changeset.foreign_key_constraint(:flagger_id)
     |> Changeset.foreign_key_constraint(:community_id)
+    |> meta_pointer_constraint()
   end
-
-  def resolve_changeset(%__MODULE__{resolved_at: nil}=flag),
-    do: Changeset.change(flag, resolved_at: DateTime.utc_now())
 
 end
--- lib/moodle_net/common/like.ex
+++ lib/moodle_net/common/like.ex
@@ -1,27 +1,40 @@
+# MoodleNet: Connecting and empowering educators worldwide
+# Copyright © 2018-2019 Moodle Pty Ltd <https://moodle.com/moodlenet/>
+# SPDX-License-Identifier: AGPL-3.0-only
 defmodule MoodleNet.Common.Like do
+  @doc """
+  A Like is an indication that an user enjoyed some content or wants
+  to find it again later.
 
-  use Ecto.Schema
-  alias MoodleNet.Common.Flag
+  Likes participate in the meta system and must be created from a pointer
+  """
+  use MoodleNet.Common.Schema
+  import MoodleNet.Common.Changeset, only: [meta_pointer_constraint: 1, change_public: 1]
+  alias MoodleNet.Common.Like
   alias MoodleNet.Meta.Pointer
-  alias MoodleNet.Communities.Community
+  alias MoodleNet.Actors.Actor
+  alias Ecto.Changeset
 
-  @primary_key {:id, :binary_id, autogenerate: false}
-  @foreign_key_type :binary_id
-  schema "mn_like" do
+  meta_schema "mn_like" do
     belongs_to :liked, Pointer
     belongs_to :liker, Actor
+    field :is_public, :boolean, virtual: true
+    field :published_at, :utc_datetime_usec
+    field :deleted_at, :utc_datetime_usec
     timestamps()
   end
 
-  @create_cast ~w(liked_id liker_id)a
+  @create_cast ~w(liked_id liker_id is_public)a
   @create_required @create_cast
-  def create_changeset(id, attrs) do
-    %Flag{id: id}
-    |> Changeset.cast(attrs, @create_cast)
+
+  def create_changeset(%Pointer{id: id}, fields) do
+    %Like{id: id}
+    |> Changeset.cast(fields, @create_cast)
     |> Changeset.validate_required(@create_required)
-    |> Changeset.foreign_key_constraint(:id)
     |> Changeset.foreign_key_constraint(:liked_id)
     |> Changeset.foreign_key_constraint(:liker_id)
+    |> meta_pointer_constraint()
+    |> change_public()
   end
 
 end
