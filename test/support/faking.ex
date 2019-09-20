520aa235299526f850a9fdf2b09d067f1bfcd01f
Antonis Kalou <kalouantonis@protonmail.com>
2019-09-20 14:17:21 +0200

    Add fake_actor! and fake_user! to Faking

--- test/support/faking.ex
+++ test/support/faking.ex
@@ -3,13 +3,25 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 defmodule MoodleNet.Test.Faking do
   alias MoodleNet.Test.Fake
-  alias MoodleNet.{Meta,Peers,Repo}
+  alias MoodleNet.{Actors,Meta,Peers,Users,Repo}
   alias MoodleNet.Peers.Peer
-  
+  alias MoodleNet.Users.User
+
   def fake_peer!(overrides \\ %{}) when is_map(overrides) do
-    pointer = Meta.point_to!(Peer)
-    {:ok, peer} = Peers.create(pointer, Fake.peer(overrides))
-    peer
+    fake_meta!(Meta.point_to(Peer), &Peers.create/2, Fake.peer(overrides))
   end
-  
+
+  def fake_actor!(overrides \\ %{}) when is_map(overrides) do
+    fake_meta!(Meta.point_to(Actor), &Actors.create/2, Fake.actor(overrides))
+  end
+
+  def fake_user!(overrides \\ %{}) when is_map(overrides) do
+    fake_meta!(Meta.point_to!(User), &Users.create/2, Fake.user(overrides))
+  end
+
+  defp fake_meta!(table_id, create_fn, params) do
+    pointer = Meta.point_to!(table_id)
+    {:ok, val} = create_fn.(pointer, params)
+    val
+  end
 end
