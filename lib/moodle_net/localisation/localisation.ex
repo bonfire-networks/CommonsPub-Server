999fcbfb06c814a4e6c0aae9f97fdeb1dd165aac
James Laver <james.laver@gmail.com>
2019-09-25 09:59:50 +0200

    Rename fetchers in M.Localisation

--- lib/moodle_net/localisation/localisation.ex
+++ lib/moodle_net/localisation/localisation.ex
@@ -1,10 +1,18 @@
 defmodule MoodleNet.Localisation do
+  @moduledoc """
+  Functions for localising the application.
+
+  Though we have chosen to call this module the more familiar name of "localisation",
+  technically, it would more accurately be called "Globalisation", which is a composite of:
+  * Localisation (l10n)
+  * Internationalisation (i18n)
+  """
 
   alias MoodleNet.Localisation.{CountryService, LanguageService}
 
-  defdelegate fetch_language(code), to: LanguageService, as: :lookup
-  defdelegate fetch_country(code), to: CountryService, as: :lookup
-  defdelegate fetch_language!(code), to: LanguageService, as: :lookup!
-  defdelegate fetch_country!(code), to: CountryService, as: :lookup!
+  defdelegate language(code), to: LanguageService, as: :lookup
+  defdelegate country(code), to: CountryService, as: :lookup
+  defdelegate language!(code), to: LanguageService, as: :lookup!
+  defdelegate country!(code), to: CountryService, as: :lookup!
   
 end
