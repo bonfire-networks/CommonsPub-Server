006a5eb0939233b37df6a4ce054b12111791580c
James Laver <james.laver@gmail.com>
2019-09-26 11:53:07 +0200

    comment revisions outline

--- lib/moodle_net/comments/comment.ex
+++ lib/moodle_net/comments/comment.ex
@@ -1,8 +1,12 @@
 defmodule MoodleNet.Comments.Comment do
 
   use MoodleNet.Common.Schema
+  alias MoodleNet.Comments.{Comment, CommentRevision, CommentLatestRevision}
 
   meta_schema "mn_comment" do
+    has_many :revisions, CommentRevision
+    has_one :latest_revision, CommentLatestRevision
+    has_one :current, through: [:latest_revision, :revision]
   end
 
 end
--- lib/moodle_net/comments/comment_revision.ex
+++ lib/moodle_net/comments/comment_revision.ex
@@ -1,9 +1,14 @@
+# MoodleNet: Connecting and empowering educators worldwide
+# Copyright © 2018-2019 Moodle Pty Ltd <https://moodle.com/moodlenet/>
+# SPDX-License-Identifier: AGPL-3.0-only
 defmodule MoodleNet.Comments.CommentRevision do
   use MoodleNet.Common.Schema
+  alias MoodleNet.Comments.Comment
 
   standalone_schema "mn_comment_revision" do
     belongs_to(:comment, Comment)
     field(:content, :string)
     timestamps(updated_at: false)
   end
+
 end
