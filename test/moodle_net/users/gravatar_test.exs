23a3a4d0462b78ff09b5f94df34f24dd100ff40a
Antonis Kalou <kalouantonis@protonmail.com>
2019-10-08 14:05:30 +0200

    Move M.GravatarTest to M.Users.GravatarTest

--- test/moodle_net/users/gravatar_test.exs
+++ test/moodle_net/users/gravatar_test.exs
@@ -3,7 +3,7 @@
 # Contains code from Pleroma <https://pleroma.social/> and CommonsPub <https://commonspub.org/>
 # SPDX-License-Identifier: AGPL-3.0-only
 
-defmodule MoodleNet.GravatarTest do
+defmodule MoodleNet.Users.GravatarTest do
   use MoodleNet.DataCase, async: true
 
   test "works" do
