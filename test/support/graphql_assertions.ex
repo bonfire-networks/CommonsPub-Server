95493582ee71b66382b5443efcd2500c33d2154e
James Laver <james.laver@gmail.com>
2019-10-11 12:16:20 +0200

    GraphQL Assertions: replace messages with default diffs, move assert_unauthorized into two different functions: assert_not_logged_in and assert_not_permitted

--- test/support/graphql_assertions.ex
+++ test/support/graphql_assertions.ex
@@ -3,25 +3,28 @@ defmodule MoodleNetWeb.Test.GraphQLAssertions do
   import ExUnit.Assertions
 
   def assert_location(loc) do
-    assert %{"column" => col, "line" => line} = loc,
-      "a location should have the correct keys"
-    assert is_integer(col) and col >= 0,
-      "a location column should be a nonzero integer"
-    assert is_integer(line) and line >= 1,
-      "a location line should be a positive integer"
+    assert %{"column" => col, "line" => line} = loc
+    assert is_integer(col) and col >= 0
+    assert is_integer(line) and line >= 1
   end
 
-  def assert_unauthorized(errs, path) do
-    assert [err] = errs,
-      "unauthorized responses should contain *one* error"
-    assert err["code"] == "unauthorized",
-      "unauthorized responses should have an unauthorized code"
-    assert err["message"] == "You need to log in first",
-      "unauthorized responses should have the correct message"
-    assert err["path"] == path,
-      "unauthorized responses should have the correct path"
-    assert [loc] = err["locations"],
-      "unauthorized responses should have one location"
+  def assert_not_logged_in(errs, path) do
+    assert [err] = errs
+    assert %{"code" => code, "message" => message} = err
+    assert %{"path" => path2, "locations" => [loc]} = err
+    assert code == "unauthorized"
+    assert message == "You need to log in first."
+    assert path == path2
+    assert_location(loc)
+  end
+
+  def assert_not_permitted(errs, path) do
+    assert [err] = errs
+    assert %{"code" => code, "message" => message} = err
+    assert %{"path" => path2, "locations" => [loc]} = err
+    assert code == "unauthorized"
+    assert message == "You do not have permission to see this."
+    assert path == path2
     assert_location(loc)
   end
 
