30cb0f42325703e699548b0d355e7cc291a9cb08
Karen Konou <konoukaren@gmail.com>
2019-10-16 10:06:14 +0200

    Remove some legacy code

--- lib/activity_pub_web/controllers/activity_pub_controller.ex
+++ lib/activity_pub_web/controllers/activity_pub_controller.ex
@@ -13,47 +13,14 @@ defmodule ActivityPubWeb.ActivityPubController do
 
   use ActivityPubWeb, :controller
 
-  import ActivityPub.Guards
-
   require Logger
 
   alias ActivityPub.Actor
   alias ActivityPub.Fetcher
   alias ActivityPub.Object
-  alias ActivityPub.SQL.Query
   alias ActivityPubWeb.ActorView
   alias ActivityPubWeb.Federator
   alias ActivityPubWeb.ObjectView
-
-  def show(conn, %{"id" => id}) do
-    id = String.to_integer(id)
-
-    case ActivityPub.get_by_local_id(id) do
-      entity when is_local(entity) ->
-        entity =
-          entity
-          |> Query.preload_aspect(:all)
-          |> Query.preload_assoc(:all)
-
-        render(conn, "show.json", entity: entity)
-
-      _ ->
-        send_resp(conn, :not_found, "")
-    end
-  end
-
-  def collection_page(conn, %{"id" => id}) do
-    id = String.to_integer(id)
-
-    case ActivityPub.get_by_local_id(id) do
-      collection when is_local(collection) and has_type(collection, "Collection") ->
-        {:ok, entity} = ActivityPub.CollectionPage.new(collection, conn.query_params)
-        render(conn, "show.json", entity: entity)
-
-      _ ->
-        send_resp(conn, :not_found, "")
-    end
-  end
 
   def object(conn, %{"uuid" => uuid}) do
     with ap_id <- Routes.activity_pub_url(conn, :object, uuid),
