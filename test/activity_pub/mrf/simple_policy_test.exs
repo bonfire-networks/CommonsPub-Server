17f046eea0226c1328a55d38f264013c60847641
Karen Konou <konoukaren@gmail.com>
2019-11-05 13:45:04 +0100

    Rewrite pleroma's copyright headers

--- test/activity_pub/mrf/simple_policy_test.exs
+++ test/activity_pub/mrf/simple_policy_test.exs
@@ -1,5 +1,6 @@
-# Pleroma: A lightweight social networking server
-# Copyright © 2019 Pleroma Authors <https://pleroma.social/>
+# MoodleNet: Connecting and empowering educators worldwide
+# Copyright © 2018-2019 Moodle Pty Ltd <https://moodle.com/moodlenet/>
+# Contains code from Pleroma <https://pleroma.social/> and CommonsPub <https://commonspub.org/>
 # SPDX-License-Identifier: AGPL-3.0-only
 
 defmodule Pleroma.Web.ActivityPub.MRF.SimplePolicyTest do
