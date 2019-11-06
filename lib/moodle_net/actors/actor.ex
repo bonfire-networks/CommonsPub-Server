9a35081b1065a9b918d96b133977fa96fc2d4510
James Laver <james.laver@gmail.com>
2019-11-06 08:58:46 +0100

    update actors for graphql

--- lib/moodle_net/actors/actor.ex
+++ lib/moodle_net/actors/actor.ex
@@ -3,10 +3,12 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 defmodule MoodleNet.Actors.Actor do
   use MoodleNet.Common.Schema
-  import MoodleNet.Common.Changeset, only: [meta_pointer_constraint: 1, change_public: 1]
+  import MoodleNet.Common.Changeset,
+    only: [meta_pointer_constraint: 1, change_public: 1, validate_language_code: 2]
   alias Ecto.Changeset
+  alias MoodleNet.{Localisation, Meta}
   alias MoodleNet.Actors.{Actor, ActorRevision, ActorLatestRevision}
-  alias MoodleNet.Meta
+  alias MoodleNet.Localisation.Language
   alias MoodleNet.Meta.Pointer
 
   alias MoodleNet.Peers.Peer
@@ -21,6 +23,7 @@ defmodule MoodleNet.Actors.Actor do
     has_one :latest_revision, ActorLatestRevision
     has_one :current, through: [:latest_revision, :revision] # poke through to revision itself
     field :preferred_username, :string
+    belongs_to :primary_language, Language, type: :string
     field :signing_key, :string
     field :is_public, :boolean, virtual: true
     field :published_at, :utc_datetime_usec
@@ -28,8 +31,13 @@ defmodule MoodleNet.Actors.Actor do
     timestamps()
   end
 
-  @create_cast ~w(peer_id alias_id preferred_username signing_key is_public)a
-  @create_required ~w(preferred_username is_public)a
+  @doc "Inflates the language (for (almost) free)"
+  def inflate(%Actor{primary_language_id: nil}=actor), do: actor
+  def inflate(%Actor{primary_language_id: lang}=actor),
+    do: %{actor | primary_language: Localisation.language!(lang)}
+
+  @create_cast ~w(peer_id primary_language_id alias_id preferred_username signing_key)a
+  @create_required ~w(preferred_username primary_language_id)a
 
   @spec create_changeset(Pointer.t(), map) :: Changeset.t()
   @doc "Creates a changeset for insertion from the given pointer and attrs"
@@ -38,12 +46,14 @@ defmodule MoodleNet.Actors.Actor do
 
     %Actor{id: id}
     |> Changeset.cast(attrs, @create_cast)
+    |> Changeset.put_change(:is_public, true)
     |> Changeset.validate_required(@create_required)
     |> Changeset.validate_format(:preferred_username, @username_regex)
     |> Changeset.unique_constraint(:alias_id)
     |> Changeset.unique_constraint(:preferred_username, # with peer
       name: "mn_actor_preferred_username_peer_id_index"
     )
+    |> validate_language_code(:primary_language_id)
     |> Changeset.unique_constraint(:preferred_username, # without peer (local)
       name: "mn_actor_peer_id_null_index"
     )
@@ -51,7 +61,7 @@ defmodule MoodleNet.Actors.Actor do
     |> change_public()
   end
 
-  @update_cast ~w(alias_id signing_key is_public)a
+  @update_cast ~w(alias_id signing_key primary_language_id)a
 
   @spec update_changeset(%Actor{}, map) :: Changeset.t()
   @doc "Creates a changeset for updating the given actor from the given attrs"
@@ -59,10 +69,12 @@ defmodule MoodleNet.Actors.Actor do
     actor
     |> Changeset.cast(attrs, @update_cast)
     |> Changeset.unique_constraint(:alias_id)
+    |> validate_language_code(:primary_language_id)
     |> meta_pointer_constraint()
     |> change_public()
   end
 
   def soft_delete_changeset(%Actor{} = actor),
     do: MoodleNet.Common.Changeset.soft_delete_changeset(actor)
+
 end
--- lib/moodle_net/actors/actors.ex
+++ lib/moodle_net/actors/actors.ex
@@ -21,39 +21,87 @@ defmodule MoodleNet.Actors do
   @doc "Fetches an actor by id"
   @spec fetch(id :: binary) :: {:ok, Actor.t()} | {:error, NotFoundError.t()}
   def fetch(id) when is_binary(id) do
-    query =
-      from(a in Actor,
-        where: a.id == ^id,
-        where: not is_nil(a.published_at)
-      )
-
-    preload_current_revision(Repo.single(query))
+    Repo.transact_with fn ->
+      with {:ok, actor} <- Repo.single(fetch_q(id)) do
+        {:ok, preload(actor)}
+      end
+    end
   end
 
-  @doc "Fetches an actor by ID, regardless if it is public or not."
+  defp fetch_q(id) do
+    from a in Actor,
+      where: a.id == ^id,
+      where: not is_nil(a.published_at),
+      where: is_nil(a.deleted_at)
+  end
+
+  defp fetch_alias_q(id) do
+    from a in Actor,
+      where: a.alias_id == ^id,
+      where: not is_nil(a.published_at),
+      where: is_nil(a.deleted_at)
+  end
+
+
+  @doc "Fetches an actor by ID, ignoring whether if it is public or not."
   @spec fetch_private(id :: binary) :: {:ok, Actor.t()} | {:error, NotFoundError.t()}
   def fetch_private(id) when is_binary(id) do
-    preload_current_revision(Repo.fetch(Actor, id))
+    with {:ok, actor} <- Repo.fetch(Actor, id) do
+      {:ok, preload(actor)}
+    end
   end
 
   # TODO: one query
   @spec fetch_by_alias(id :: binary) :: {:ok, Actor.t()} | {:error, NotFoundError.t()}
-  def fetch_by_alias(alias_id) when is_binary(alias_id),
-    do: preload_current_revision(Repo.fetch_by(Actor, alias_id: alias_id))
+  def fetch_by_alias(alias_id) when is_binary(alias_id) do
+    with {:ok, actor} <- Repo.single(fetch_alias_q(alias_id)) do
+      {:ok, preload(actor)}
+    end
+  end
+
+  @spec fetch_by_alias(id :: binary) :: {:ok, Actor.t()} | {:error, NotFoundError.t()}
+  def fetch_by_alias_private(alias_id) when is_binary(alias_id) do
+    with {:ok, actor} <- Repo.fetch_by(Actor, %{alias_id: alias_id}) do
+      {:ok, preload(actor)}
+    end
+  end
 
   # TODO: one query
   @doc "Fetches an actor by username"
   @spec fetch(username :: binary) :: {:ok, Actor.t()} | {:error, NotFoundError.t()}
-  def fetch_by_username(username) when is_binary(username),
-    do: preload_current_revision(Repo.fetch_by(Actor, preferred_username: username))
+  def fetch_by_username(username) when is_binary(username) do
+    with {:ok, actor} <- Repo.single(fetch_by_username_q(username)) do
+      {:ok, preload(actor)}
+    end
+  end
 
-  defp preload_current_revision({:ok, actor}), do: {:ok, Repo.preload(actor, :current)}
-  defp preload_current_revision(error), do: error
+  defp fetch_by_username_q(username) do
+    from a in Actor,
+      where: a.preferred_username == ^username,
+      where: is_nil(a.peer_id),
+      where: is_nil(a.deleted_at),
+      where: not is_nil(a.published_at)
+  end
+
+  # a username remains taken forever and regardless of publicity
+  defp is_username_available_q(username) do
+    from a in Actor,
+      where: a.preferred_username == ^username,
+      where: is_nil(a.peer_id)
+  end
+
+  defp preload(actor) do
+    actor
+    |> Actor.inflate()
+    |> preload_current_revision()
+  end
+
+  defp preload_current_revision(actor), do: Repo.preload(actor, :current)
 
   @doc "true if the provided preferred_username is available to register"
   @spec is_username_available?(username :: binary) :: boolean()
   def is_username_available?(username) when is_binary(username) do
-    case fetch_by_username(username) do
+    case Repo.single(is_username_available_q(username)) do
       {:ok, _} -> false
       _ -> true
     end
@@ -67,6 +115,7 @@ defmodule MoodleNet.Actors do
 
       with {:ok, actor} <- Repo.insert(Actor.create_changeset(actor_pointer, attrs)),
            {:ok, revision} <- Revision.insert(ActorRevision, actor, attrs) do
+        actor = Actor.inflate(actor)
         latest_revision = ActorLatestRevision.forge(revision)
         {:ok, %Actor{actor | latest_revision: latest_revision, current: revision}}
       end
@@ -77,10 +126,9 @@ defmodule MoodleNet.Actors do
           {:ok, Actor.t()} :: {:error, Changeset.t()}
   def create_with_alias(alias_id, attrs) when is_map(attrs) do
     Repo.transact_with(fn ->
-      with {:ok, actor} <- create(attrs) do
-        actor
-        |> Changeset.change(alias_id: alias_id)
-        |> Repo.update()
+      with {:ok, actor} <- create(attrs),
+           {:ok, actor} <- Repo.update(Changeset.change(actor, alias_id: alias_id)) do
+        {:ok, Actor.inflate(actor)}
       end
     end)
   end
@@ -90,6 +138,7 @@ defmodule MoodleNet.Actors do
     Repo.transact_with(fn ->
       with {:ok, actor} <- Repo.update(Actor.update_changeset(actor, attrs)),
            {:ok, revision} <- Revision.insert(ActorRevision, actor, attrs) do
+        actor = Actor.inflate(actor)
         latest_revision = ActorLatestRevision.forge(revision)
         {:ok, %Actor{actor | latest_revision: latest_revision, current: revision}}
       end
@@ -97,12 +146,15 @@ defmodule MoodleNet.Actors do
   end
 
   def soft_delete(%Actor{} = actor) do
-    Repo.update(Actor.soft_delete_changeset(actor))
+    with {:ok, actor} <- Repo.update(Actor.soft_delete_changeset(actor)) do
+      {:ok, Actor.inflate(actor)}
+    end
   end
 
   @spec delete(actor :: Actor.t()) :: {:ok, Actor.t()} | {:error, term}
   def delete(%Actor{} = actor) do
-    # should cascade delete
-    Repo.delete(actor)
+    with {:ok, actor} <- Repo.delete(actor) do
+      {:ok, Actor.inflate(actor)}
+    end
   end
 end
