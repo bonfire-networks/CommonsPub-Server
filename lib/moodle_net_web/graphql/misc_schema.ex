d1ff84b93a07a2953ce226d57b44b17cb8c1fdc2
James Laver <james.laver@gmail.com>
2019-11-15 10:48:32 +0100

    hopefully fix metadata scraper endpoint

--- lib/moodle_net_web/graphql/misc_schema.ex
+++ lib/moodle_net_web/graphql/misc_schema.ex
@@ -7,7 +7,6 @@ defmodule MoodleNetWeb.GraphQL.MiscSchema do
   """
   use Absinthe.Schema.Notation
 
-  alias MoodleNetWeb.GraphQL.Errors
   alias ActivityPub.Fetcher
 
   import_types(MoodleNetWeb.Schema.Types.Custom.JSON)
@@ -31,11 +30,9 @@ defmodule MoodleNetWeb.GraphQL.MiscSchema do
   end
 
   def fetch_web_metadata(%{url: url}, info) do
-    with {:ok, _actor} <- current_actor(info) do
-      case MoodleNet.MetadataScraper.fetch(url) do
-        {:error, _} -> Errors.bad_gateway_error()
-        ret -> ret
-      end
+    case MoodleNet.MetadataScraper.fetch(url) do
+      {:error, _} -> {:error, MoodleNet.Common.NotFoundError.new(url)}
+      ret -> ret
     end
   end
 
@@ -54,19 +51,4 @@ defmodule MoodleNetWeb.GraphQL.MiscSchema do
     end
   end
 
-  @doc """
-  FIXME: repeated code from moodlenetschema
-  """
-  defp current_user(%{context: %{current_user: nil}}), do: Errors.unauthorized_error()
-  defp current_user(%{context: %{current_user: user}}), do: {:ok, user}
-
-  defp current_actor(info) do
-    case current_user(info) do
-      {:ok, user} ->
-        {:ok, user.actor}
-
-      ret ->
-        ret
-    end
-  end
 end
