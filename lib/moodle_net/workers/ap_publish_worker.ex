ad5790d49fd730c5ee23b6052c2fc8022ce42675
Antonis Kalou <kalouantonis@protonmail.com>
2019-11-19 11:57:22 +0100

    Add checks for is_local in APPublishWorker

--- lib/moodle_net/workers/ap_publish_worker.ex
+++ lib/moodle_net/workers/ap_publish_worker.ex
@@ -18,31 +18,44 @@ defmodule MoodleNet.Workers.APPublishWorker do
   def perform(%{"context_id" => context_id}, _job) do
     context = context_id |> Meta.find!() |> Meta.follow!()
 
-    {:ok, _} = publish(context)
+    # ignore allow failure for now
+    _ = publish(context)
   end
 
-  defp publish(%Comment{} = comment), do: ignore_deleted(comment, &Publisher.comment/1)
-  defp publish(%Resource{} = resource), do: ignore_deleted(resource, &Publisher.create_resource/1)
-  defp publish(%Community{} = community), do: ignore_deleted(community, &Publisher.create_community/1)
   defp publish(%Collection{} = collection), do: ignore_deleted(collection, &Publisher.create_collection/1)
-  defp publish(%Flag{} = flag), do: ignore_deleted(flag, &Publisher.flag/1)
 
-  defp publish(%Follow{} = follow) do
+  defp publish(%Comment{is_local: true} = comment) do
+    ignore_deleted(comment, &Publisher.comment/1)
+  end
+
+  defp publish(%Resource{is_local: true} = resource) do
+    ignore_deleted(resource, &Publisher.create_resource/1)
+  end
+
+  defp publish(%Community{is_local: true} = community) do
+    ignore_deleted(community, &Publisher.create_community/1)
+  end
+
+  defp publish(%Follow{is_local: true} = follow) do
     publish_deleted(follow, &Publisher.follow/1, &Publisher.unfollow/1)
   end
 
-  defp publish(%Block{} = block) do
+  defp publish(%Flag{is_local: true} = flag) do
+    ignore_deleted(flag, &Publisher.flag/1)
+  end
+
+  defp publish(%Block{is_local: true} = block) do
     publish_deleted(block, &Publisher.block/1, &Publisher.unblock/1)
   end
 
-  defp publish(%Like{} = like) do
+  defp publish(%Like{is_local: true} = like) do
     publish_deleted(like, &Publisher.like/1, &Publisher.unlike/1)
   end
 
-  defp publish(%{__struct__: type}) do
-    Logger.warn("Unsupported type for AP publisher: #{to_string(type)}")
+  defp publish(context) do
+    Logger.warn("Unsupported type for AP publisher: #{context.id}")
 
-    {:error, {:unsupported_type, type}}
+    {:error, :unsupported}
   end
 
   defp publish_deleted(%{deleted_at: deleted_at} = context, default_fn, _deleted_fn) when is_nil(deleted_at) do
