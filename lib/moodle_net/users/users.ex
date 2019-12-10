84bc296c715f1d1cb8be26d50a393db7c592f819
James Laver <james.laver@gmail.com>
2019-12-10 12:47:05 +0100

    fix typo

--- lib/moodle_net/users/users.ex
+++ lib/moodle_net/users/users.ex
@@ -331,7 +331,7 @@ defmodule MoodleNet.Users do
 
   def inbox(%User{inbox_id: inbox_id}=user, opts \\ %{}) do
     Repo.transaction(fn ->
-      subs = [user.inbox_id | Feeds.active_sub_for(user)]
+      subs = [user.inbox_id | Feeds.active_subs_for(user)]
       Feeds.subs_feed(subs)
     end)
   end
