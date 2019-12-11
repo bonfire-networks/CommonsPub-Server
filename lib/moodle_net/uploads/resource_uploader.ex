ebd2fc124144e6ec78f7edce042dc3d6e323964a
Antonis Kalou <kalouantonis@protonmail.com>
2019-12-11 13:46:44 +0100

    Add support for default MN extensions

--- lib/moodle_net/uploads/resource_uploader.ex
+++ lib/moodle_net/uploads/resource_uploader.ex
@@ -4,7 +4,10 @@
 defmodule MoodleNet.Uploads.ResourceUploader do
   use MoodleNet.Uploads.Definition
 
-  def allowed_extensions, do: ~w(pdf rtf ogg mp3 flac wav flv gif jpg jpeg png)
+  def allowed_extensions,
+    do: ~w(pdf rtf docx doc odt ott xls xlsx ods ots csv ppt pps pptx odp otp) ++
+      ~w(odg otg odc ogg mp3 m4a wav mp4 flv avi gif jpg jpeg png svg webm) ++
+      ~w(eps tex mbz)
 
   def transform(file) do
     :skip
