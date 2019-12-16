d0d00f1d805e5f6d59a6938dd270ea94ba5b334a
James Laver <james.laver@gmail.com>
2019-12-16 18:48:39 +0100

    add featured communities and collections to graphql

--- lib/moodle_net_web/graphql/instance_resolver.ex
+++ lib/moodle_net_web/graphql/instance_resolver.ex
@@ -3,7 +3,9 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 defmodule MoodleNetWeb.GraphQL.InstanceResolver do
 
-  alias MoodleNet.{Common, Fake, GraphQL, Instance}
+  alias MoodleNet.{Common, Fake, Features, GraphQL, Repo, Instance}
+  alias MoodleNet.Collections.Collection
+  alias MoodleNet.Communities.Community
 
   def instance(_, info) do
     hostname  = System.get_env("HOSTNAME")
@@ -11,8 +13,26 @@ defmodule MoodleNetWeb.GraphQL.InstanceResolver do
     {:ok, %{hostname: hostname, description: description}}
   end
 
+  def featured_communities(_, args, info) do
+    Repo.transact_with(fn ->
+      features = Features.list(%{contexts: [Community]})
+      count = Enum.count(features)
+      # count = Features.count_for_list(%{contexts: [Community]})
+      {:ok, GraphQL.edge_list(features, count)}
+    end)
+  end
+
+  def featured_collections(_, args, info) do
+    Repo.transact_with(fn ->
+      features = Features.list(%{contexts: [Collection]})
+      count = Enum.count(features)
+      # count = Features.count_for_list(%{contexts: [Collection]})
+      {:ok, GraphQL.edge_list(features, count)}
+    end)
+  end
+
   def outbox(_, args, info) do
-    # Repo.transact_with(fn ->
+    Repo.transact_with(fn ->
       # activities =
       #   Instance.outbox()
       #   |> Enum.map(fn box -> %{cursor: box.id, node: box.activity} end)
@@ -21,7 +41,7 @@ defmodule MoodleNetWeb.GraphQL.InstanceResolver do
       count = 0
       page_info = Common.page_info(activities)
       {:ok, %{page_info: page_info, total_count: count, edges: activities}}
-    # end)
+    end)
   end
 
 end
