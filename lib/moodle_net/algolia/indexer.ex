f96e16f7fa965a7a3993657adc013644d5e1ad10
Karen Konou <konoukaren@gmail.com>
2020-01-25 11:44:33 +0100

    Transform objects to algolia structure

---
+++ lib/moodle_net/algolia/indexer.ex
@@ -0,0 +1,83 @@
+# MoodleNet: Connecting and empowering educators worldwide
+# Copyright © 2018-2019 Moodle Pty Ltd <https://moodle.com/moodlenet/>
+# SPDX-License-Identifier: AGPL-3.0-only
+
+defmodule MoodleNet.Algolia.Indexer do
+  alias MoodleNet.Repo
+  alias MoodleNet.Follows.FollowerCounts
+  alias MoodleNet.Likes.LikerCounts
+
+  def format_community(community) do
+    follower_count = case FollowerCounts.one(context_id: community.id) do
+      {:ok, struct} -> struct.count
+      {:error, _} -> nil
+    end
+
+    %{
+      "id" => community.id,
+      "canonicalUrl" => community.actor.canonical_url,
+      "followers" => %{
+        "totalCount" => follower_count
+      },
+      "icon" => Map.get(community, :icon),
+      "image" => Map.get(community, :image),
+      "name" => community.name,
+      "preferredUsername" => community.actor.preferred_username,
+      "summary" => Map.get(community, :summary),
+      "index_type" => "Community",
+      "index_instance" => System.get_env("HOSTNAME", MoodleNetWeb.Endpoint.host()),
+      "createdAt" => community.published_at
+    }
+  end
+
+  def format_collection(collection) do
+    collection = MoodleNet.Repo.preload(collection, [community: [:actor]])
+
+    follower_count = case FollowerCounts.one(context_id: collection.id) do
+      {:ok, struct} -> struct.count
+      {:error, _} -> nil
+    end
+
+    %{
+      "id" => collection.id,
+      "canonicalUrl" => collection.actor.canonical_url,
+      "followers" => %{
+        "totalCount" => follower_count
+      },
+      "icon" => Map.get(collection, :icon),
+      "name" => collection.name,
+      "preferredUsername" => collection.actor.preferred_username,
+      "summary" => Map.get(collection, :summary),
+      "index_type" => "Collection",
+      "index_instance" => System.get_env("HOSTNAME", MoodleNetWeb.Endpoint.host()),
+      "createdAt" => collection.published_at,
+      "community" => format_community(collection.community)
+    }
+  end
+
+  def format_resource(resource) do
+    resource = Repo.preload(resource, [collection: [actor: [], community: [actor: []]]])
+
+    likes_count = case LikerCounts.one(context_id: resource.id) do
+      {:ok, struct} -> struct.count
+      {:error, _} -> nil
+    end
+
+    %{
+      "id" => resource.id,
+      "name" => resource.name,
+      "canonicalUrl" => resource.canonical_url,
+      "createdAt" => resource.published_at,
+      "icon" => Map.get(resource, :icon),
+      "licence" => Map.get(resource, :licence),
+      "likes" => %{
+        "totalCount" => likes_count
+      },
+      "summary" => Map.get(resource, :summary),
+      "updatedAt" => resource.updated_at,
+      "index_type" => "Resource",
+      "index_instance" => System.get_env("HOSTNAME", MoodleNetWeb.Endpoint.host()),
+      "collection" => format_collection(resource.collection)
+    }
+  end
+end
---
+++ test/moodle_net/algolia/indexer_test.exs
@@ -0,0 +1,52 @@
+# MoodleNet: Connecting and empowering educators worldwide
+# Copyright © 2018-2019 Moodle Pty Ltd <https://moodle.com/moodlenet/>
+# SPDX-License-Identifier: AGPL-3.0-only
+
+defmodule MoodleNet.Algolia.IndexerTest do
+  use MoodleNet.DataCase
+  alias MoodleNet.Algolia.Indexer
+  import MoodleNet.Test.Faking
+
+  test "format community" do
+    community = fake_user!() |> fake_community!()
+    json = Indexer.format_community(community)
+    assert json["id"] == community.id
+    assert json["canonicalUrl"] == community.actor.canonical_url
+    assert json["icon"] == community.icon
+    assert json["image"] == community.image
+    assert json["preferredUsername"] == community.actor.preferred_username
+    assert json["summary"] == community.summary
+    assert json["index_type"] == "Community"
+  end
+
+  test "format collection" do
+    user = fake_user!()
+    community = fake_community!(user)
+    collection = fake_collection!(user, community)
+
+    json = Indexer.format_collection(collection)
+    assert json["id"] == collection.id
+    assert json["canonicalUrl"] == collection.actor.canonical_url
+    assert json["icon"] == collection.icon
+    assert json["preferredUsername"] == collection.actor.preferred_username
+    assert json["summary"] == collection.summary
+    assert json["index_type"] == "Collection"
+    assert is_map(json["community"])
+  end
+
+  test "format resource" do
+    user = fake_user!()
+    community = fake_community!(user)
+    collection = fake_collection!(user, community)
+    resource = fake_resource!(user, collection)
+
+    json = Indexer.format_resource(resource)
+    assert json["id"] == resource.id
+    assert json["canonicalUrl"] == resource.canonical_url
+    assert json["icon"] == resource.icon
+    assert json["summary"] == resource.summary
+    assert json["index_type"] == "Resource"
+    assert is_map(json["collection"])
+    assert is_map(json["collection"]["community"])
+  end
+end
