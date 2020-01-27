f0784663cab141ed4b0e1ba7d5242b69917fc84e
Antonis Kalou <kalouantonis@protonmail.com>
2020-01-27 16:18:34 +0100

    Make url an optional field for Resource

--- lib/moodle_net/resources/resource.ex
+++ lib/moodle_net/resources/resource.ex
@@ -30,8 +30,8 @@ defmodule MoodleNet.Resources.Resource do
     timestamps()
   end
 
-  @required ~w(name url)a
-  @cast @required ++ ~w(canonical_url is_public is_disabled license summary icon)a
+  @required ~w(name)a
+  @cast @required ++ ~w(canonical_url is_public is_disabled license summary icon url)a
 
   @spec create_changeset(User.t(), Collection.t(), map) :: Changeset.t()
   @doc "Creates a changeset for insertion of a resource with the given attributes."
