7c98f484083f076415773758eb841cb42257c87c
Mayel <git@mayel.space>
2020-02-21 09:43:33 +0100

    Revert "Revert "Revert "Add unique index for ap_ids"""
    This reverts commit 278d4329f6eeb53588ecefe6cbac8ba318ab7d9d.

--- priv/repo/migrations/20200218135114_add_unique_ap_id_index.exs
+++
@@ -1,8 +0,0 @@
-defmodule MoodleNet.Repo.Migrations.AppUniqueApIdIndex do
-  use Ecto.Migration
-
-  def change do
-    drop index(:ap_object, ["(data->>'id')"])
-    create unique_index(:ap_object, ["(data->>'id')"])
-  end
-end
