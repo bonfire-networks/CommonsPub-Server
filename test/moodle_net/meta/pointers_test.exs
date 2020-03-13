7a3f42b75b13ecaed141a461ad79b58a8a015253
Antonis Kalou <kalouantonis@protonmail.com>
2020-03-13 13:41:23 +0100

    Split TableService and Pointers tests

--- test/moodle_net/meta/pointers_test.exs
+++ test/moodle_net/meta/pointers_test.exs
@@ -1,93 +1,21 @@
-defmodule MoodleNet.MetaTest do
+# MoodleNet: Connecting and empowering educators worldwide
+# Copyright © 2018-2019 Moodle Pty Ltd <https://moodle.com/moodlenet/>
+# SPDX-License-Identifier: AGPL-3.0-only
+defmodule MoodleNet.Meta.PointersTest do
   use ExUnit.Case, async: true
 
   import ExUnit.Assertions
-  import MoodleNet.Meta.Introspection, only: [ecto_schema_table: 1]
   import MoodleNet.Test.Faking
   alias MoodleNet.{Access, Repo}
 
   alias MoodleNet.Meta.{
-    Pointer,
     Pointers,
-    Table,
-    TableService,
     TableNotFoundError
   }
 
-  alias MoodleNet.Access.{RegisterEmailAccess, RegisterEmailDomainAccess}
-  alias MoodleNet.Activities.Activity
-  alias MoodleNet.Communities.Community
-  alias MoodleNet.Collections.Collection
-  alias MoodleNet.Resources.Resource
-  alias MoodleNet.Threads.{Comment, Thread}
-
-  alias MoodleNet.Blocks.Block
-  alias MoodleNet.Flags.Flag
-  alias MoodleNet.Follows.Follow
-  alias MoodleNet.Features.Feature
-  alias MoodleNet.Likes.Like
-  alias MoodleNet.Common.NotInTransactionError
-  alias MoodleNet.Feeds.Feed
   alias MoodleNet.Peers.Peer
-  alias MoodleNet.Users.User
-  alias MoodleNet.Localisation.{Country, Language}
 
-  @known_schemas [
-    Table, Feature, Feed, Peer, User, Community, Collection, Resource, Comment,
-    Thread, Flag, Follow, Like, Country, Language, RegisterEmailAccess,
-    RegisterEmailDomainAccess, Block, Activity,
-  ]
-  @known_tables Enum.map(@known_schemas, &ecto_schema_table/1)
-  @table_schemas Map.new(Enum.zip(@known_tables, @known_schemas))
-  @expected_table_names Enum.sort(@known_tables)
-
-  describe "MoodleNet.Meta.Pointers.TableService" do
-    setup do
-      :ok = Ecto.Adapters.SQL.Sandbox.checkout(MoodleNet.Repo)
-      {:ok, %{}}
-    end
-
-    test "is fetching from good source data" do
-      in_db =
-        Repo.all(Table)
-        |> Enum.map(& &1.table)
-        |> Enum.sort()
-
-      assert @expected_table_names == in_db
-    end
-
-    @bad_table_names ["fizz", "buzz bazz"]
-
-    test "returns results consistent with the source data" do
-      # the database will be our source of truth
-      tables = Repo.all(Table)
-      assert Enum.count(tables) == Enum.count(@expected_table_names)
-      # Every db entry must match up to our module metadata
-      for t <- tables do
-        assert %{id: id, table: table} = t
-        # we must know about this schema to pair it up
-        assert schema = Map.fetch!(@table_schemas, table)
-        assert schema in @known_schemas
-        t2 = %{t | schema: schema}
-        # There are 3 valid keys, 3 pairs of functions to check
-        for key <- [schema, table, id] do
-          assert {:ok, t2} == TableService.lookup(key)
-          assert {:ok, id} == TableService.lookup_id(key)
-          assert {:ok, schema} == TableService.lookup_schema(key)
-          assert t2 == TableService.lookup!(key)
-          assert id == TableService.lookup_id!(key)
-          assert schema == TableService.lookup_schema!(key)
-        end
-      end
-
-      for t <- @bad_table_names do
-        assert {:error, %TableNotFoundError{table: t}} == TableService.lookup(t)
-        assert %TableNotFoundError{table: t} == catch_throw(TableService.lookup!(t))
-      end
-    end
-  end
-
-  describe "MoodleNet.Pointers.forge!" do
+  describe "forge!" do
     setup do
       :ok = Ecto.Adapters.SQL.Sandbox.checkout(MoodleNet.Repo)
       {:ok, %{}}
@@ -124,7 +52,7 @@ defmodule MoodleNet.MetaTest do
     end
   end
 
-  describe "MoodleNet.Meta.Pointers.follow" do
+  describe "follow" do
     setup do
       :ok = Ecto.Adapters.SQL.Sandbox.checkout(MoodleNet.Repo)
       {:ok, %{}}
@@ -199,17 +127,66 @@ defmodule MoodleNet.MetaTest do
         assert pointer8.id == pointer4.id
         assert Map.drop(pointer5.pointed, [:is_disabled]) == Map.drop(peer, [:is_disabled])
         assert Map.drop(pointer6.pointed, [:is_disabled]) == Map.drop(peer2, [:is_disabled])
-        pointed7 = Map.drop(pointer7.pointed, [:actor, :local_user, :email_confirm_tokens, :is_disabled, :is_public, :is_deleted, :canonical_url, :is_local, :preferred_username])
-        user3 = Map.drop(user, [:actor, :local_user, :email_confirm_tokens, :is_disabled, :is_public, :is_deleted, :canonical_url, :is_local, :preferred_username])
+
+        pointed7 =
+          Map.drop(pointer7.pointed, [
+            :actor,
+            :local_user,
+            :email_confirm_tokens,
+            :is_disabled,
+            :is_public,
+            :is_deleted,
+            :canonical_url,
+            :is_local,
+            :preferred_username
+          ])
+
+        user3 =
+          Map.drop(user, [
+            :actor,
+            :local_user,
+            :email_confirm_tokens,
+            :is_disabled,
+            :is_public,
+            :is_deleted,
+            :canonical_url,
+            :is_local,
+            :preferred_username
+          ])
+
         assert pointed7 == user3
-        pointed8 = Map.drop(pointer8.pointed, [:actor, :local_user, :email_confirm_tokens, :is_disabled, :is_public, :is_deleted, :canonical_url, :is_local, :preferred_username])
-        user4 = Map.drop(user2, [:actor, :local_user, :email_confirm_tokens, :is_disabled, :is_public, :is_deleted, :canonical_url, :is_local, :preferred_username])
+
+        pointed8 =
+          Map.drop(pointer8.pointed, [
+            :actor,
+            :local_user,
+            :email_confirm_tokens,
+            :is_disabled,
+            :is_public,
+            :is_deleted,
+            :canonical_url,
+            :is_local,
+            :preferred_username
+          ])
+
+        user4 =
+          Map.drop(user2, [
+            :actor,
+            :local_user,
+            :email_confirm_tokens,
+            :is_disabled,
+            :is_public,
+            :is_deleted,
+            :canonical_url,
+            :is_local,
+            :preferred_username
+          ])
+
         assert pointed8 == user4
       end)
     end
 
     test "key error does not occur for missing ID's" do
-      
     end
   end
 end
