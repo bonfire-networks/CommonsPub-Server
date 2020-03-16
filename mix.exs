2c44d852b9dfe63a28983abf7a164314b2233ff3
James Laver <james.laver@gmail.com>
2020-03-16 18:46:30 +0100

    bump versions of deps

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule MoodleNet.Mixfile do
     [
       app: :moodle_net,
       version: "0.9.6-dev",
-      elixir: "~> 1.9.0",
+      elixir: "~> 1.10.0",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
@@ -57,8 +57,8 @@ defmodule MoodleNet.Mixfile do
 
   defp deps do
     [ # graphql
-      {:absinthe, "~> 1.5.0-beta.2"},
-      {:absinthe_plug, "~> 1.5.0-alpha.0"},
+      {:absinthe, "~> 1.5.0-rc.3"},
+      {:absinthe_plug, "~> 1.5.0-rc.2"},
       # webserver
       {:cowboy, "~> 2.6"},
       {:plug_cowboy, "~> 2.1"},
@@ -69,8 +69,9 @@ defmodule MoodleNet.Mixfile do
       {:phoenix_pubsub, "~> 1.1"},
       {:phoenix_html, "~> 2.13"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_integration, "~> 0.6.0"},
+      {:phoenix_integration, "~> 0.8.0"},
       {:phoenix_ecto, "~> 4.0"},
+      {:floki, "~> 0.24.0", override: true},
       # File storage
       {:belt, git: "https://gitlab.com/kalouantonis/belt"},
       # File format parsing
