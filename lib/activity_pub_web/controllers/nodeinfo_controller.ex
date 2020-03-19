25aba97780f7f78721e70d4c521aec0866e5aa19
Karen Konou <konoukaren@gmail.com>
2020-03-19 14:10:46 +0100

    Implement basic nodeinfo

--- lib/activity_pub_web/controllers/nodeinfo_controller.ex
+++ lib/activity_pub_web/controllers/nodeinfo_controller.ex
@@ -25,22 +25,56 @@ defmodule ActivityPubWeb.NodeinfoController do
     json(conn, response)
   end
 
+  def raw_nodeinfo do
+    %{
+      version: "2.0",
+      software: %{
+        name: Mix.Project.config()[:name],
+        version: Mix.Project.config()[:version]
+      },
+      protocols: ["ActivityPub"],
+      services: %{
+        inbound: [],
+        outbound: []
+      },
+      openRegistrations: Config.get([MoodleNet.Users, :public_registration]),
+      usage: %{},
+      metadata: %{
+        nodeName: Config.get([:instance, :name]),
+        nodeDescription: Config.get([:instance, :description]),
+        federation: Config.get([:instance, :federating])
+      }
+    }
+  end
+
   def nodeinfo(conn, %{"version" => "2.0"}) do
     conn
     |> put_resp_header(
       "content-type",
       "application/json; profile=http://nodeinfo.diaspora.software/ns/schema/2.0#; charset=utf-8"
     )
-    |> json(%{"content" => "nil"})
+    |> json(raw_nodeinfo())
   end
 
   def nodeinfo(conn, %{"version" => "2.1"}) do
+    raw_response = raw_nodeinfo()
+
+    updated_software =
+      raw_response
+      |> Map.get(:software)
+      |> Map.put(:repository, Mix.Project.config()[:source_url])
+
+    response =
+      raw_response
+      |> Map.put(:software, updated_software)
+      |> Map.put(:version, "2.1")
+
     conn
     |> put_resp_header(
       "content-type",
       "application/json; profile=http://nodeinfo.diaspora.software/ns/schema/2.1#; charset=utf-8"
     )
-    |> json(%{"content" => "nil"})
+    |> json(response)
   end
 
   def nodeinfo(conn, _) do
