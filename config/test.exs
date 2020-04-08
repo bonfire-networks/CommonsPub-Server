a20ae1a614c86e97a398df8bd06da58fbf89c916
Mayel <git@mayel.space>
2020-04-08 11:23:44 +0200

    attempt geo mutation

--- config/test.exs
+++ config/test.exs
@@ -36,6 +36,7 @@ config :moodle_net, MoodleNet.Mail.MailService,
   adapter: Bamboo.TestAdapter
 
 config :moodle_net,
+  app_name: "MoodleNet",
   base_url: "http://localhost:4001",
   ap_base_path: System.get_env("AP_BASE_PATH", "/pub"),
   frontend_base_url: System.get_env("FRONTEND_BASE_URL", "http://localhost:3000")
