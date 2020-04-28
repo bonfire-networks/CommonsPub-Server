381688aca416690343102604741b9afe9c2b8ae6
Mayel <git@mayel.space>
2020-04-28 11:16:32 +0200

    comment more

--- test/measurement/unit_test.exs
+++ test/measurement/unit_test.exs
@@ -1,35 +1,35 @@
-# MoodleNet: Connecting and empowering educators worldwide
-# Copyright © 2018-2019 Moodle Pty Ltd <https://moodle.com/moodlenet/>
-# SPDX-License-Identifier: AGPL-3.0-only
-defmodule Measurement.UnitTest do
-  # @tag :skip
-  use MoodleNetWeb.ConnCase, async: true
-  import MoodleNetWeb.Test.Automaton
+# # MoodleNet: Connecting and empowering educators worldwide
+# # Copyright © 2018-2019 Moodle Pty Ltd <https://moodle.com/moodlenet/>
+# # SPDX-License-Identifier: AGPL-3.0-only
+# defmodule Measurement.UnitTest do
+#   # @tag :skip
+#   use MoodleNetWeb.ConnCase, async: true
+#   import MoodleNetWeb.Test.Automaton
 
-  import MoodleNetWeb.Test.GraphQLAssertions
-  import MoodleNetWeb.Test.GraphQLFields
-  import MoodleNet.Test.Trendy
-  import MoodleNet.Test.Faking
-  import Grumble
-  import Zest
+#   import MoodleNetWeb.Test.GraphQLAssertions
+#   import MoodleNetWeb.Test.GraphQLFields
+#   import MoodleNet.Test.Trendy
+#   import MoodleNet.Test.Faking
+#   import Grumble
+#   import Zest
 
-  import Measurement.Test.Faking
+#   import Measurement.Test.Faking
 
 
-  describe "unit" do
+#   describe "unit" do
 
-    @tag :skip
-    test "works for the owner, randoms, admins and guests" do
-      [alice, bob] = some_fake_users!(%{}, 2)
-      comm = fake_community!(alice)
-      item = unit!(alice, comm)
-      conns = [user_conn(alice), user_conn(bob), json_conn()]
-      vars = %{id: item.id}
-      for conn <- conns do
-        unit2 = grumble_post_key(unit_query(), conn, :unit, vars)
-        assert_unit(unit2)
-      end
-    end
+#     @tag :skip
+#     test "works for the owner, randoms, admins and guests" do
+#       [alice, bob] = some_fake_users!(%{}, 2)
+#       comm = fake_community!(alice)
+#       item = unit!(alice, comm)
+#       conns = [user_conn(alice), user_conn(bob), json_conn()]
+#       vars = %{id: item.id}
+#       for conn <- conns do
+#         unit2 = grumble_post_key(unit_query(), conn, :unit, vars)
+#         assert_unit(unit2)
+#       end
+#     end
 
-  end
-end
+#   end
+# end
