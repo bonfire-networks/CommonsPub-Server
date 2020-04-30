a639f73497010212708f01476d1eccde3685afb3
Mayel <git@mayel.space>
2020-04-30 10:22:41 +0200

    Revert "Merge branch 'extension/valueflows' into develop"
    This reverts commit 7f220654c8910a92f02b2fcf9de32bda9d22deed, reversing
    changes made to d4f0b85be3547d52c572e1c7205b35c572e8ac0b.

--- priv/repo/migrations/20200423105739_vf_intent.exs
+++
@@ -1,9 +0,0 @@
-defmodule MoodleNet.Repo.Migrations.VF.Intent do
-  use Ecto.Migration
-
-  def change do
-      ValueFlows.Knowledge.Migrations.change_action()
-      ValueFlows.Planning.Migrations.change_intent()
-  end
-
-end
