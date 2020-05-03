a4bee0df9d047e8bb42eb083bb91df809323e6ec
Mayel <git@mayel.space>
2020-05-03 14:02:31 +0200

    improve taxonomy schema

--- lib/taxonomy/sql/tags.schema.sql
+++ lib/taxonomy/sql/tags.schema.sql
@@ -6,9 +6,10 @@ CREATE TABLE "taxonomy_tags" (
     "parent_tag_id" integer,
     "description" text,
     CONSTRAINT "tag_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "tag_parent" FOREIGN KEY (parent_tag_id) REFERENCES taxonomy_tags(id) ON UPDATE CASCADE ON DELETE SET NULL NOT DEFERRABLE,
 ) WITH (oids = false);
 
-CREATE INDEX "tag_5e6faac5245d8" ON "taxonomy_tags" USING btree ("parent_tag_id");
+CREATE INDEX "taxonomy_tags_parent_tag_id_index" ON "taxonomy_tags" USING btree ("parent_tag_id");
 
 
 DROP TABLE IF EXISTS "taxonomy_tags_related" CASCADE;
@@ -19,6 +20,6 @@ CREATE TABLE "taxonomy_tags_related" (
     CONSTRAINT "tags_related_tag_id_fkey" FOREIGN KEY (tag_id) REFERENCES taxonomy_tags(id) ON UPDATE CASCADE ON DELETE CASCADE NOT DEFERRABLE
 ) WITH (oids = false);
 
-CREATE INDEX "tags_related_5e6fac453a415" ON "taxonomy_tags_related" USING btree ("tag_id");
+CREATE INDEX "taxonomy_tags_related_tag_id_index" ON "taxonomy_tags_related" USING btree ("tag_id");
 
-CREATE INDEX "tags_related_5e6fac4f713dc" ON "taxonomy_tags_related" USING btree ("related_tag_id");
+CREATE INDEX "taxonomy_tags_related_related_tag_id_index" ON "taxonomy_tags_related" USING btree ("related_tag_id");
