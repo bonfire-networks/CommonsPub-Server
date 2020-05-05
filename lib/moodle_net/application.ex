b6e6b9faf7cd49d154c4f6ef587491041ae3e83f
Karen Konou <konoukaren@gmail.com>
2020-05-05 13:50:32 +0200

    Fix oban logging

--- lib/moodle_net/application.ex
+++ lib/moodle_net/application.ex
@@ -27,7 +27,7 @@ defmodule MoodleNet.Application do
 
     {:ok, _} = Logger.add_backend(Sentry.LoggerBackend)
     :ok = Oban.Telemetry.attach_default_logger(:debug)
-    :ok = :telemetry.attach("oban-logger", [:oban, :failure], &MoodleNet.Workers.ObanLogger.handle_event/4, nil)
+    :ok = :telemetry.attach("oban-logger", [:oban, :job, :exception], &MoodleNet.Workers.ObanLogger.handle_event/4, nil)
 
     # TODO: better supervision tree. LS, CS and TS only need repo on
     # startup, never need restarting, but they should require repo to
