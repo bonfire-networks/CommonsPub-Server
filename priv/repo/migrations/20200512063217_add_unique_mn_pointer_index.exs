0b9f329ed3ac8218a7580f33558b6ee607037572
Karen Konou <konoukaren@gmail.com>
2020-05-12 06:53:06 +0000

    Revert "Add unique mn_pointer_id index"
    This reverts commit d0f22cea2d8928e1e0ed26c734805dc5c3b71ae3

--- priv/repo/migrations/20200512063217_add_unique_mn_pointer_index.exs
+++
@@ -1,8 +0,0 @@
-defmodule MoodleNet.Repo.Migrations.AddMnPointerIndex do
-  use Ecto.Migration
-
-  def change do
-    drop index(:ap_object, :mn_pointer_id)
-    create unique_index(:ap_object, :mn_pointer_id)
-  end
-end
