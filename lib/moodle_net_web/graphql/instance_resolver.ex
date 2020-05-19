a401405f767bdce59fcddae9fcc6d2131d148528
Antonis Kalou <kalouantonis@protonmail.com>
2020-05-19 13:21:41 +0200

    Add context preload for instance activity

--- lib/moodle_net_web/graphql/instance_resolver.ex
+++ lib/moodle_net_web/graphql/instance_resolver.ex
@@ -73,7 +73,7 @@ defmodule MoodleNetWeb.GraphQL.InstanceResolver do
         query: Activities.Activity,
         page_opts: page_opts,
         base_filters: [deleted: false, feed_timeline: feed_id, table: tables],
-        data_filters: [page: [desc: [created: page_opts]]],
+        data_filters: [page: [desc: [created: page_opts]], preload: :context],
       }          
     )
   end
