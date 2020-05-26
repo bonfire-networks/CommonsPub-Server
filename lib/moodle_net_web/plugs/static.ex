1727211cffff16b160b2fd23a6f6e7e572d39006
James Laver <james.laver@gmail.com>
2020-05-26 12:07:32 +0200

    static plug

--- lib/moodle_net_web/plugs/static.ex
+++ lib/moodle_net_web/plugs/static.ex
@@ -6,26 +6,25 @@ defmodule MoodleNetWeb.Plugs.Static do
   alias Plug.Static
 
   def init(opts) do
+    config = Application.fetch_env!(:moodle_net, MoodleNet.Uploads)
+    from = Keyword.fetch!(config, :directory)
+    at = Keyword.fetch!(config, :path)
     %{
       gzip?: Keyword.get(opts, :gzip, false),
       brotli?: Keyword.get(opts, :brotli, false),
-      only: Keyword.get(opts, :only, []),
-      prefix: Keyword.get(opts, :only_matching, []),
+      only_rules: Keyword.get(opts, :only, {[], []}),
       qs_cache: Keyword.get(opts, :cache_control_for_vsn_requests, "public, max-age=31536000"),
       et_cache: Keyword.get(opts, :cache_control_for_etags, "public"),
       et_generation: Keyword.get(opts, :etag_generation, nil),
       headers: Keyword.get(opts, :headers, %{}),
       content_types: Keyword.get(opts, :content_types, %{}),
+      from: from,
+      at: Plug.Router.Utils.split(at),
     }
+    
   end
 
   def call(conn, opts) do
-    config = Application.fetch_env!(:moodle_net, MoodleNet.Uploads)
-    at = Keyword.fetch!(config, :path)
-    from = Keyword.fetch!(config, :directory)
-    IO.inspect(static_from: from)
-    more = %{at: at, from: from}
-    opts = Map.merge(opts, more)
     Static.call(conn, opts)
   end
 
