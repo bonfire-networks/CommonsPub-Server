e095b75660d91dd0c98cd7bc01eb537287b07a55
Mayel <git@mayel.space>
2020-06-02 14:53:06 +0200

    de-duplicate scope (mesurement and VF)

--- lib/measurement/hydration.ex
+++ lib/measurement/hydration.ex
@@ -7,7 +7,7 @@ defmodule Measurement.Hydration do
 
   def hydrate(blueprint) do
     %{
-      accounting_scope: [
+      unit_context: [
         resolve_type: &__MODULE__.resolve_context_type/2,
       ],
       unit: %{
