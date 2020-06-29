1d0939edd40e770fe89daf689aa502447747e85e
ivan minutillo <bernini@inventati.org>
2020-06-29 23:00:12 +0200

    UI hero

--- lib/moodle_net_web/live/components/hero_profile/hero_profile_live.ex
+++ lib/moodle_net_web/live/components/hero_profile/hero_profile_live.ex
@@ -31,11 +31,13 @@ defmodule MoodleNetWeb.Component.HeroProfileLive do
           <div class="info__icon">
             <img alt="profile pic" src="<%= @user.icon_url %>" />
           </div>
-          <div class="info__name"><%= @user.name %></div>
-          <div class="info__username">@<%= e(@user, :actor, :preferred_username, "") %></div>
-          <div class="info__details">
-            <div class="details__meta"><a href="<%= @user.website %>" target="_blank"><i class="feather-external-link"></i> <%= e(@user, :website_friendly, "") %></a></div>
-            <div class="details__meta"><i class="feather-map-pin"></i><%= @user.location %></div>
+          <div class="info__meta">
+            <div class="info__name"><%= @user.name %></div>
+            <div class="info__username">@<%= e(@user, :actor, :preferred_username, "") %></div>
+            <div class="info__details">
+              <div class="details__meta"><a href="<%= @user.website %>" target="_blank"><i class="feather-external-link"></i> <%= e(@user, :website_friendly, "") %></a></div>
+              <div class="details__meta"><i class="feather-map-pin"></i><%= @user.location %></div>
+            </div>
           </div>
         </div>
       </div>
