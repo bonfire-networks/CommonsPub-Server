fe1b57d9f847188cd8b8803f60b5ccae9e67c3b0
Mayel <git@mayel.space>
2020-06-30 12:05:28 +0200

    hacks to use ResolvePage outside of Absinthe

--- lib/moodle_net/graphql/resolve_pages.ex
+++ lib/moodle_net/graphql/resolve_pages.ex
@@ -7,19 +7,25 @@ defmodule MoodleNet.GraphQL.ResolvePages do
 
   @enforce_keys [:module, :fetcher, :context, :page_opts, :info]
   defstruct [
-    :module, :fetcher, :context, :page_opts, :info,
+    :module,
+    :fetcher,
+    :context,
+    :page_opts,
+    :info,
     cursor_validators: [&Ecto.ULID.cast/1],
     single_opts: %{default_limit: 5, max_limit: 15},
     batch_opts: %{default_limit: 3, max_limit: 5},
     deep_opts: %{default_limit: 3, max_limit: 3},
-    getter_fn: &Fields.getter/1,
+    getter_fn: &Fields.getter/1
   ]
 
   alias MoodleNet.GraphQL
   alias MoodleNet.GraphQL.ResolvePages
   import Absinthe.Resolution.Helpers, only: [async: 1, batch: 3]
 
-  def run(%ResolvePages{info: info}=rp) do
+  def run(%ResolvePages{info: info} = rp) do
+    IO.inspect(depth: GraphQL.list_depth(info))
+    IO.inspect(info: Map.take(info, [:context]))
     run(GraphQL.list_depth(info), Map.take(info, [:context]), rp)
   end
 
@@ -27,11 +33,18 @@ defmodule MoodleNet.GraphQL.ResolvePages do
   defp run(1, info, rp), do: run_limit(rp, info, rp.batch_opts)
   defp run(_other, info, rp), do: run_limit(rp, info, rp.deep_opts)
 
+  # when running in Absinthe, do it async
+  defp run_full(%{context: %{schema: schema}} = rp, info, opts) do
+    with {:ok, opts} <- GraphQL.full_page_opts(rp.page_opts, rp.cursor_validators, opts) do
+      async(fn ->
+        apply(rp.module, rp.fetcher, [opts, Map.take(info, [:context]), rp.context])
+      end)
+    end
+  end
+
   defp run_full(rp, info, opts) do
     with {:ok, opts} <- GraphQL.full_page_opts(rp.page_opts, rp.cursor_validators, opts) do
-      async fn ->
-        apply(rp.module, rp.fetcher, [opts, Map.take(info, [:context]), rp.context])
-      end
+      apply(rp.module, rp.fetcher, [opts, Map.take(info, [:context]), rp.context])
     end
   end
 
@@ -39,25 +52,27 @@ defmodule MoodleNet.GraphQL.ResolvePages do
     cond do
       function_exported?(rp.module, rp.fetcher, 2) -> batch_limit(rp, info, opts)
       function_exported?(rp.module, rp.fetcher, 3) -> unbatch_limit(rp, info, opts)
-      true -> throw {:missing_fetcher, {rp.module, rp.fetcher, [2,3]}}
+      true -> throw({:missing_fetcher, {rp.module, rp.fetcher, [2, 3]}})
     end
   end
 
   # this is the fast path, we've actually written the scary query
   defp batch_limit(rp, info, opts) do
     with {:ok, opts} <- GraphQL.limit_page_opts(rp.page_opts, opts) do
-      batch {rp.module, rp.fetcher, {opts, Map.take(info, [:context])}},
-        rp.context, rp.getter_fn.(rp.context)
+      batch(
+        {rp.module, rp.fetcher, {opts, Map.take(info, [:context])}},
+        rp.context,
+        rp.getter_fn.(rp.context)
+      )
     end
   end
 
   # this is the slow path, we should write the scary query
   defp unbatch_limit(rp, info, opts) do
     with {:ok, opts} <- GraphQL.limit_page_opts(rp.page_opts, opts) do
-      async fn ->
+      async(fn ->
         apply(rp.module, rp.fetcher, [opts, Map.take(info, [:context]), rp.context])
-      end
+      end)
     end
   end
-
 end
