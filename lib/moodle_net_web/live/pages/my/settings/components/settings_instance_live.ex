7e72d225de7d26ad37ff147310527905bc02a3c5
Mayel <git@mayel.space>
2020-07-29 16:30:52 +0200

    accesslist

--- lib/moodle_net_web/live/pages/my/settings/components/settings_instance_live.ex
+++ lib/moodle_net_web/live/pages/my/settings/components/settings_instance_live.ex
@@ -8,13 +8,13 @@ defmodule MoodleNetWeb.SettingsLive.SettingsInstanceLive do
     <section class="settings__section">
         <div class="section__main">
           <h1>Customize your instance</h1>
-          <form action="#" phx-submit="post">
+          <form action="#" phx-submit="add-domain">
             <div class="section__item">
-              <h4>Add domain to allowlist</h4>
-              <input name="name" type="text" placeholder="Enter domain (e.g. riseup.org)">
+              <h4>Allow anyone to sign up who has an email address from a particular domain</h4>
+              <input name="domain" type="text" placeholder="Enter domain (e.g. riseup.org)">
             </div>
             <div class="section__actions">
-              <button type="submit" phx-disable-with="Updating...">Update</button>
+              <button type="submit" phx-disable-with="Saving...">Add</button>
             </div>
         </form>
         </div>
