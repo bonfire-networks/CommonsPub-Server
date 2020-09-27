2ae84aaf2fc7ec4292583d6c9d630dec036d63b3
Mayel <git@mayel.space>
2020-09-27 09:08:08 +0200

    module override

--- lib/mixology/utils/module_override.ex
+++ lib/mixology/utils/module_override.ex
@@ -2,8 +2,8 @@ defmodule ModuleOverride do
   @doc """
   Clone the existing module under a new name
   """
-  def archive_module(replace_module, module_source_file) do
-    Code.ensure_compiled(replace_module)
+  def archive_module(module_source_file, extending_module) do
+    Code.ensure_compiled(extending_module)
 
     with {:ok, f} <- File.read(module_source_file) do
       Code.eval_string(String.replace(f, "defmodule ", "defmodule ModuleOverride."))
