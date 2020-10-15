976e38feda3441613d6564aeadfbbc30cdee12b4
Mayel <git@mayel.space>
2020-10-15 12:13:44 +0200

    quickfix for name reservation

--- lib/extensions/characters/name_reservation.ex
+++ lib/extensions/characters/name_reservation.ex
@@ -4,15 +4,24 @@ defmodule CommonsPub.Characters.NameReservation do
 
   alias __MODULE__
 
-  @primary_key {:id, Cloak.Ecto.SHA256, autogenerate: false}
+  @primary_key {:id, :binary, autogenerate: false}
   schema "actor_name_reservation" do
+    field(:name, :string)
     timestamps(type: :utc_datetime_usec, inserted_at: :created_at, updated_at: false)
   end
 
   def changeset(name) when is_binary(name) do
-    Changeset.change(%NameReservation{}, id: name)
+    Changeset.change(%NameReservation{}, id: :crypto.hash(:sha256, name) |> Base.encode16, name: name)
     |> Changeset.unique_constraint(:name,
       name: "actor_name_reservation_pkey"
     )
   end
+
+  # FIXME
+  # def changeset(name) when is_binary(name) do
+  #   Changeset.change(%NameReservation{}, id: name)
+  #   |> Changeset.unique_constraint(:name,
+  #     name: "actor_name_reservation_pkey"
+  #   )
+  # end
 end
