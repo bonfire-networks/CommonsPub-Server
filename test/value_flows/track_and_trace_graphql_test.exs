d5e3e42a8289bc7d915e604285fde64201c32a9b
bernini <bernini@inventati.org>
2020-10-27 16:05:22 +0100

    track and trace concat graphql queries

---
+++ test/value_flows/track_and_trace_graphql_test.exs
@@ -0,0 +1,130 @@
+defmodule ValueFlows.TrackAndTraceGraphQLTest do
+  use CommonsPub.Web.ConnCase, async: true
+
+  import CommonsPub.Test.Faking
+  import CommonsPub.Tag.Simulate
+
+  import CommonsPub.Utils.{Trendy, Simulation}
+  import ValueFlows.Simulate
+  import Measurement.Simulate
+  import Geolocation.Simulate
+
+  import ValueFlows.Test.Faking
+
+  alias ValueFlows.Observation.EconomicEvent.EconomicEvents
+  alias ValueFlows.Observation.EconomicResource.EconomicResources
+
+  @schema CommonsPub.Web.GraphQL.Schema
+
+  describe "Trace" do
+    test "3 level nesting" do
+      user = fake_user!()
+      resource = fake_economic_resource!(user)
+      other_resource = fake_economic_resource!(user)
+      process = fake_process!(user)
+      output_event = fake_economic_event!(user, %{
+        output_of: process.id,
+        resource_inventoried_as: resource.id,
+        action: "produce"
+      })
+      input_event = fake_economic_event!(user, %{
+        input_of: process.id,
+        resource_inventoried_as: other_resource.id,
+        action: "use"
+      })
+      query = """
+        query ($id: ID) {
+        economicResource(id: $id) {
+          id
+          trace {
+            id
+            trace {
+              __typename
+              ... on Process {
+                id
+                trace {
+                  id
+                  trace {
+                    __typename
+                  }
+                }
+              }
+              ... on EconomicResource {
+                id
+                trace {
+                  id
+                  trace {
+                    __typename
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+      """
+      assert {:ok, %{data: result}} = Absinthe.run(query, @schema, variables: %{"id" => resource.id})
+      assert result["economicResource"]["id"] == resource.id
+      assert hd(result["economicResource"]["trace"])["id"] == output_event.id
+      assert hd(hd(result["economicResource"]["trace"])["trace"])["id"] == process.id
+      assert hd(hd(hd(result["economicResource"]["trace"])["trace"])["trace"])["id"] == input_event.id
+      assert hd(hd(hd(hd(result["economicResource"]["trace"])["trace"])["trace"])["trace"])["__typename"] == "EconomicResource"
+    end
+  end
+
+  describe "Track" do
+    test "3 level nesting" do
+      user = fake_user!()
+      resource = fake_economic_resource!(user)
+      other_resource = fake_economic_resource!(user)
+      process = fake_process!(user)
+      output_event = fake_economic_event!(user, %{
+        output_of: process.id,
+        resource_inventoried_as: resource.id,
+        action: "produce"
+      })
+      input_event = fake_economic_event!(user, %{
+        input_of: process.id,
+        resource_inventoried_as: other_resource.id,
+        action: "use"
+      })
+      query = """
+        query ($id: ID) {
+        economicResource(id: $id) {
+          id
+          track {
+            id
+            track {
+              __typename
+              ... on Process {
+                id
+                track {
+                  id
+                  track {
+                    __typename
+                  }
+                }
+              }
+              ... on EconomicResource {
+                id
+                track {
+                  id
+                  track {
+                    __typename
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+      """
+      assert {:ok, %{data: result}} = Absinthe.run(query, @schema, variables: %{"id" => other_resource.id})
+      assert result["economicResource"]["id"] == other_resource.id
+      assert hd(result["economicResource"]["track"])["id"] == input_event.id
+      assert hd(hd(result["economicResource"]["track"])["track"])["id"] == process.id
+      assert hd(hd(hd(result["economicResource"]["track"])["track"])["track"])["id"] == output_event.id
+      assert hd(hd(hd(hd(result["economicResource"]["track"])["track"])["track"])["track"])["__typename"] == "EconomicResource"
+    end
+  end
+end
