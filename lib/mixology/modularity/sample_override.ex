3d6974da7a5f07c6257a1773c45c9c7a11b8c888
Mayel <git@mayel.space>
2020-11-20 10:44:15 +0100

    cleanup

--- lib/mixology/modularity/sample_override.ex
+++ lib/mixology/modularity/sample_override.ex
@@ -1,28 +1,24 @@
-# module that we will override
-alias CommonsPub.Utils.Simulation, as: NewModule
-# new name for the old module
-alias Original.CommonsPub.Utils.Simulation, as: ArchiveModule
+# start by archiving the old module
+Modularity.Module.Override.clone(CommonsPub.Utils.Simulation, Original.CommonsPub.Utils.Simulation)
 
-# archive the old module
-Modularity.Module.Override.clone(NewModule, ArchiveModule)
-
-defmodule NewModule do
+defmodule CommonsPub.Utils.Simulation do
   require Modularity.Module.Extend
+  alias Original.CommonsPub.Utils.Simulation, as: Original
 
   # extend the archived module
-  Modularity.Module.Extend.extends ArchiveModule
+  Modularity.Module.Extend.extends Original
 
   ####
-  # (Re)define new or existing functions
+  # Redefine existing functions, or add new ones:
   ####
 
   # example of straight up replacing a function
   def name(), do: Faker.Person.last_name()
 
   # example of modifying the input of a function
-  # def maybe_one_of(list), do: list ++ [""] |> ArchiveModule.maybe_one_of()
+  # def maybe_one_of(list), do: list ++ [""] |> Original.maybe_one_of()
 
   # example of modifying the output of a function
-  def location(), do: ArchiveModule.location() |> String.replace(",", " -")
+  def location(), do: Original.location() |> String.replace(",", " -")
 
 end
