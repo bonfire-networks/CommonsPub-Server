6bb23652e23a9f1298299d359857859ad52a7532
Mayel <git@mayel.space>
2020-12-12 15:35:00 +0100

    add config

--- config/bonfire_valueflows.exs
+++ config/bonfire_valueflows.exs
@@ -4,5 +4,6 @@ config :bonfire_valueflows,
   web_module: CommonsPub.Web,
   repo_module: CommonsPub.Repo,
   user_schema: Pointers.Pointer, # FIXME
-  org_schema: Pointers.Pointer # FIXME
+  org_schema: Pointers.Pointer, # FIXME
+  valid_agent_schemas: [Bonfire.Data.Identity.User, CommonsPub.Users.User, Pointers.Pointer]
   # all_types: []
